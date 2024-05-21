7ac7452e922a9f59707922c4e564e4849c6f914d
Moeed Sarwar <167990538+moeeduxbert@users.noreply.github.com>
2024-05-21 15:48:59 +0500

    story: added story for header file

--- app/src/assets/svgs/svg.ts
+++ app/src/assets/svgs/svg.ts
@@ -3,8 +3,5 @@ import BackArrow from './arrow-left.svg';
 import Global from './global.svg';
 import DelinkSvg from './logout.svg';
 
-export {
-  BackArrow,
-  Global,
-  DelinkSvg
-};
+export { BackArrow, Global, DelinkSvg };
+
