3198cb162de98ec1bd5eca7cd29a60e0ddeae8ed
shakeelmazhar10 <shakeel.c@uxbert.com>
2024-05-22 13:04:03 +0500

    fix: home screen component name

--- app/src/navigation/tab-navigation.tsx
+++ app/src/navigation/tab-navigation.tsx
@@ -1,5 +1,5 @@
 import { screenNames } from '@app/navigation/screen-names.navigation';
-import Home from '@app/screens/home-screen/home.screen';
+import Home from '@app/screens/home/home.screen';
 import Profile from '@app/screens/profile/profile.screen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
