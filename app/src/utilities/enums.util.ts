866a908ce78c501aff72200bb558847b7b47166d
mariauxbert <165198119+mariauxbert@users.noreply.github.com>
2024-05-26 15:55:12 +0500

    Feature: Create picker button component (#8)
    * chore: update readme file
    
    * feat: typography, color schemes and text variants
    
    This branch includes Inter fonts, all text variants and all color schemes
    
    * style : added stories for text components
    
    * feat: chip component
    
    * ops: ndk version matched with boilerplate
    
    * style : added stories for chip component
    
    * style : added stories for chip component
    
    * feat: banner
    
    * refactor: updated test file for banner
    
    * feat: add picker button
    
    * story: added story for picker button
    
    * chore: add moment package with exact version for date manipulation
    
    * fix: home screen
    
    * fix: home screen component name
    
    * refactor: remove extra input components and add other palette in theme
    
    * fix: spell correction for secondary
    
    * fix: Ipay view to acccept other react node
    
    * refactor: accept single styles.
    
    * refactor: view accept single style
    
    * refactor: add fallback date
    
    * refactor: change moderate size to scale size
    
    ---------
    
    Co-authored-by: Abdul Rehman <rehman.c@uxbert.com>
    Co-authored-by: shakeelmazhar10 <shakeel.c@uxbert.com>
    Co-authored-by: Moeed Sarwar <167990538+moeeduxbert@users.noreply.github.com>

--- app/src/utilities/enums.util.ts
+++ app/src/utilities/enums.util.ts
@@ -10,6 +10,24 @@ export enum variants {
   COLORED = 'colored'
 }
 
+/**
+ * Defines variants types used for timePeriod.
+ */
+export enum dayPeriod {
+  AM = 'AM',
+  PM = 'PM'
+}
+
+/**
+ * Defines variants types used for picker button.
+ */
+export enum pickerVariant {
+  Date = 'date',
+  Text = 'text',
+  Time = 'time',
+  DateAndTime = 'dateAndTime'
+}
+
 export enum inputVariants {
   CURRENCY = 'Currency',
   PHONE_NUMBER = 'PhoneNumber'
