a28403d7bb20baaaea365d1a28e9b0409e37a3bb
mariauxbert <165198119+mariauxbert@users.noreply.github.com>
2024-05-26 20:30:43 +0500

    Feature: Create Tabs Component (#24)
    * chore: update readme file
    
    * feat: typography, color schemes and text variants
    
    This branch includes Inter fonts, all text variants and all color schemes
    
    * style : added stories for text components
    
    * feat: chip component
    
    * ops: ndk version matched with boilerplate
    
    * style : added stories for chip component
    
    * style : added stories for chip component
    
    * feat: banner
    
    * refactor: updated test file for banner
    
    * feat: Add header component
    
    * story: added story for header file
    
    * feat: create tab bar
    
    * refactor: Add enum in styles
    
    * fix: home screen
    
    * fix: home screen component name
    
    * style: added storybook for tabs component
    
    * refactor: remove extra input components and add other palette in theme
    
    * fix: spell correction for secondary
    
    * fix: Ipay view to acccept other react node
    
    * refactor: accept single styles.
    
    * refactor: view accept single style
    
    * docs: updated docs for molecules
    
    * refactor: moderate style to scalesize
    
    ---------
    
    Co-authored-by: Abdul Rehman <rehman.c@uxbert.com>
    Co-authored-by: shakeelmazhar10 <shakeel.c@uxbert.com>
    Co-authored-by: Moeed Sarwar <167990538+moeeduxbert@users.noreply.github.com>

--- app/src/components/molecules/ipay-tabs/ipay-tabs.component.tsx
+++ app/src/components/molecules/ipay-tabs/ipay-tabs.component.tsx
@@ -1,58 +1,50 @@
-
 import { RNFootnoteText, RNPressable, RNView } from '@app/components/atoms';
+import { TabBase } from '@app/utilities/enums';
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet, ViewStyle } from 'react-native';
+import { ScrollView, ViewStyle } from 'react-native';
 import { IPayTabsProps } from './ipay-tabs.interface';
 import { generateStyles } from './ipay-tabs.style';
-import { TabBase } from '@app/utilities/enums';
 
 const IPayTabs: React.FC<IPayTabsProps> = ({
-    tabs,
-    onSelect,
-    scrollable = false,
-    variant = TabBase.Natural, 
-    customStyles,
+  tabs,
+  onSelect,
+  scrollable = false,
+  variant = TabBase.Natural,
+  customStyles
 }) => {
-    const [selectedTab, setSelectedTab] = useState<string | null>(tabs[0]);
-    const styles = generateStyles(variant); // Generate styles based on variant
+  const [selectedTab, setSelectedTab] = useState<string | null>(tabs[0]);
+  const styles = generateStyles(variant); // Generate styles based on variant
 
-    const handleTabClick = (tab: string) => {
-        setSelectedTab(tab);
-        onSelect && onSelect();
-    };
+  const handleTabClick = (tab: string) => {
+    setSelectedTab(tab);
+    onSelect && onSelect();
+  };
 
-    const getTabStyle = (isSelected: boolean) => [
-        styles.tab,
-        isSelected ? styles.selectedTab : styles.unSelectedTab, //{ backgroundColor: colors.primary.primary500 } : { backgroundColor: colors.primaryOverlay },
-        !scrollable && styles.flexTab,
-    ];
+  const getTabStyle = (isSelected: boolean) => [
+    styles.tab,
+    isSelected ? styles.selectedTab : styles.unSelectedTab, //{ backgroundColor: colors.primary.primary500 } : { backgroundColor: colors.primaryOverlay },
+    !scrollable && styles.flexTab
+  ];
 
-    return (
-        <RNView style={[styles.container, customStyles as ViewStyle]}>
-            <ScrollView
-                horizontal={scrollable}
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={styles.scrollContainer}
-            >
-
-                {tabs.map((tab) => (
-                    <RNPressable
-                        key={tab}
-                        style={getTabStyle(tab === selectedTab)}
-                        onPress={() => handleTabClick(tab)}
-                    >
-                        <RNFootnoteText
-                            style={tab === selectedTab ? styles.selected : styles.unselected}
-                            text={tab}
-                            regular={tab === selectedTab ? false : true}
-                        />
-                    </RNPressable>
-                ))}
-           
-               
-            </ScrollView>
-        </RNView>
-    );
+  return (
+    <RNView style={[styles.container, customStyles as ViewStyle]}>
+      <ScrollView
+        horizontal={scrollable}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.scrollContainer}
+      >
+        {tabs.map((tab) => (
+          <RNPressable key={tab} style={getTabStyle(tab === selectedTab)} onPress={() => handleTabClick(tab)}>
+            <RNFootnoteText
+              style={tab === selectedTab ? styles.selected : styles.unselected}
+              text={tab}
+              regular={tab === selectedTab ? false : true}
+            />
+          </RNPressable>
+        ))}
+      </ScrollView>
+    </RNView>
+  );
 };
 
 export default IPayTabs;
