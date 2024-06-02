83dd3b8d75b54270df46ae69d89e3c6e58bb78d1
Moeed Sarwar <moeed.c@uxbert.com>
2024-06-02 22:59:40 +0500

    fixed no swapfile

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,4 +4,7 @@ vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = ""
 vim.opt.winbar = "%=%m %f"
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
 vim.o.showtabline = 0
