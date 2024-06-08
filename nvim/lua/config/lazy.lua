711c2521d0a55f072e4136092b76ce6ed1050095
Moeed Sarwar <moeed.c@uxbert.com>
2024-06-09 04:14:23 +0500

    added lackluster

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,12 +14,13 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
     -- add okuuva/auto-save.nvim plugin
+    -- example lazy.nvim install setup
     {
       "okuuva/auto-save.nvim",
       cmd = "ASToggle", -- optional for lazy loading on command
@@ -64,7 +65,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "habamax", "tokyonight" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
