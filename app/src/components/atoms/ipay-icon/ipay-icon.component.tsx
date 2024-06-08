fe5a25a2167ccc52dc5942ab7284de154ebf862e
mariauxbert <maria.c@uxbert.com>
2024-06-08 10:48:40 +0500

    fix: lint issue fix

--- app/src/components/atoms/ipay-icon/ipay-icon.component.tsx
+++ app/src/components/atoms/ipay-icon/ipay-icon.component.tsx
@@ -4,11 +4,11 @@ import IPaySvgIcon from './ipay-svg-icon';
 import { IpayIconProps } from './ipay-svg-icon.interface';
 /**
  * IPayIcon
- * 
+ *
  * A component that renders an icon using `Svg` and `Path` components from `react-native-svg`.
  * This component wraps the `IPaySvgIcon` component, providing a consistent interface for rendering icons
  * from the provided design system added in IconSet.
- * 
+ *
  * @param {object} props - The component's props.
  * @param {string} props.icon - The name of the icon to render. This should match the `name` property in the icon set.
  * @param {string | number} [props.size] - The size of the icon. Can be a string (e.g., '24px') or a number (e.g., 24).
@@ -17,10 +17,9 @@ import { IpayIconProps } from './ipay-svg-icon.interface';
  * @param {boolean} [props.native] - Indicates if the component is being used in a React Native environment.
  * @param {JSX.ElementConstructor<any>} [props.SvgComponent] - The component to use for the SVG element, defaults to 'svg'.
  * @param {JSX.ElementConstructor<any>} [props.PathComponent] - The component to use for the path element, defaults to 'path'.
- * 
+ *
  * @returns {JSX.Element} The rendered `IPaySvgIcon` component.
  */
-
 
 const IPayIcon: React.FC<IpayIconProps> = ({
   icon,
@@ -28,10 +27,11 @@ const IPayIcon: React.FC<IpayIconProps> = ({
   disableFill = false,
   removeInlineStyle = false,
   color,
- 
+  testID,
 }: IpayIconProps): JSX.Element => {
   return (
     <IPaySvgIcon
+      testID={`${testID}-ipay-icon`}
       icon={icon}
       native
       SvgComponent={Svg}
