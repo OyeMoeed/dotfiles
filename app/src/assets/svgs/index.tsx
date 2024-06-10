92b607e53e412955d859d5bfa0f06e3aaf4bd6d8
mariauxbert <maria.c@uxbert.com>
2024-06-10 15:47:50 +0500

    refactor: remove extra icons

--- app/src/assets/svgs/index.tsx
+++ app/src/assets/svgs/index.tsx
@@ -5,47 +5,21 @@ import Shield from '@assets/svgs/shield.svg';
 import TickMark from '@assets/svgs/tick-mark.svg';
 import FlagsIcon from './Flags.svg';
 import LogoIcon from './Logo_icon.svg';
-import RequestMoney from './RequestMoney.svg';
-import CalculatorIcon from './calculator.svg';
-import CardIcon from './card.svg';
-import Card from './card1.svg';
-import Gift from './gift.svg';
-import Global from './global.svg';
-import GlobalIcon from './global1.svg';
 import HeartIcon from './heart.svg';
-import InfoCircle from './info-circle.svg';
-import LeftListIcon from './left-list.icon.svg';
 import LogoBlueIcon from './logoIconBlue.svg';
-import ReceiptItem from './receipt-item.svg';
 import ReceiptIcon from './receipt-item1.svg';
-import ScannerIcon from './scanner.svg';
 import SendMoneyIcon from './send-money-icon.svg';
-import SendIcon from './send-money.svg';
-import UserTagIcon from './user-tag.svg';
 
 export {
-  CalculatorIcon,
-  Card,
-  CardIcon,
   FallbackImage,
   FallbackLoader,
   FlagsIcon,
-  Gift,
-  Global,
-  GlobalIcon,
   HeartIcon,
-  InfoCircle,
-  LeftListIcon,
   Logo,
   LogoBlueIcon,
   LogoIcon,
   ReceiptIcon,
-  ReceiptItem,
-  RequestMoney,
-  ScannerIcon,
-  SendIcon,
   SendMoneyIcon,
   Shield,
   TickMark,
-  UserTagIcon,
 };
