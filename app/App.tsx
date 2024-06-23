272c8e7a9b53dda1a57726496e9c0a34dffaac2f
rehman-uxbert <154500489+rehman-uxbert@users.noreply.github.com>
2024-06-23 17:40:56 +0500

    Develop (#204)
    * Hotfix: add support for custom style in ipay list component (#196)
    
    * hotfix: add support for custom style
    
    * fix: lint fix
    
    * hotfix: add support for left aligh text (#197)
    
    * test: setup test file
    
    * hotfix: toast for notifications (#183)
    
    * hotfix: toast for notifications
    
    * fix: merge conflicts
    
    * fix: import statement
    
    ---------
    
    Co-authored-by: talibhrafique <talib.c@uxbert.com>
    Co-authored-by: mariauxbert <maria.c@uxbert.com>
    Co-authored-by: shakeelmazhar10 <168355956+shakeelmazhar10@users.noreply.github.com>

--- app/App.tsx
+++ app/App.tsx
@@ -5,21 +5,35 @@
  * @format
  */
 
+import { ToastProvider } from '@app/components/molecules/ipay-toast/context/ipay-toast-context';
 import MainNavigation from '@app/navigation/app-navigator.navigation';
-
 import { persistor, store } from '@store/store';
 import React from 'react';
+import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
 
+  const backgroundStyle = {
+    flex: 1,
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
+  };
 
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <MainNavigation />
-      
+        <ToastProvider>
+          <SafeAreaView style={backgroundStyle}>
+            <StatusBar
+              barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+              backgroundColor={backgroundStyle.backgroundColor}
+            />
+            <MainNavigation />
+          </SafeAreaView>
+        </ToastProvider>
       </PersistGate>
     </Provider>
   );
