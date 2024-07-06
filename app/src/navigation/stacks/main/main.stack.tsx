acadce08af9f33b92c1cabbc206951f16b60f139
moeeduxbert <167990538+moeeduxbert@users.noreply.github.com>
2024-07-06 20:13:54 +0500

    Update main.stack.tsx

--- app/src/navigation/stacks/main/main.stack.tsx
+++ app/src/navigation/stacks/main/main.stack.tsx
@@ -21,7 +21,6 @@ import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useEffect } from 'react';
 import TabNavigation from '../../tab-navigation';
-import IPayCardIssueBottomSheet from '@app/components/templates/ipay-card-issue-bottomsheet/ipay-card-issue-bottomsheet';
 
 const MainStack = createStackNavigator();
 
@@ -35,7 +34,7 @@ const MainStackNavigator = () => {
   return (
     <IPaySafeAreaView>
       <MainStack.Navigator screenOptions={{ headerShown: false }}>
-        <MainStack.Screen name={screenNames.HOME_BASE} options={{ headerShown: false }} component={IPayCardIssueBottomSheet} />
+                <MainStack.Screen name={screenNames.HOME_BASE} options={{ headerShown: false }} component={TabNavigation} />
         <MainStack.Group screenOptions={{ presentation: 'card', headerMode: 'float', animationTypeForReplace: 'push' }}>
           <MainStack.Screen name={screenNames.WALLET} component={Wallet} />
           <MainStack.Screen name={screenNames.TOP_UP} component={TopUp} />
