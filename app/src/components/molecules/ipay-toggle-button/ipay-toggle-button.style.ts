7bee66b60ec3df228fb12e645608a2be5de7be32
JavedUxbert <154338382+JavedUxbert@users.noreply.github.com>
2024-07-07 11:41:13 +0500

    fix: refactor toggle button component

--- app/src/components/molecules/ipay-toggle-button/ipay-toggle-button.style.ts
+++ app/src/components/molecules/ipay-toggle-button/ipay-toggle-button.style.ts
@@ -1,55 +1,41 @@
 import createStyleSheet from '@app/styles/scaled-sheet.styles';
 import { spacing } from '@app/styles/spacing.const';
+import { moderateScale } from 'react-native-size-matters';
 
 const toggleButtonStyles = (colors: any) =>
   createStyleSheet({
     container: {
-      width: spacing.CUSTOME_SCALE(40),
-      height: spacing.CUSTOME_SCALE(20),
+      width: moderateScale(51),
+      height: moderateScale(31),
       borderRadius: spacing.SCALE_24,
       justifyContent: 'center',
       paddingHorizontal: spacing.SCALE_1,
     },
     childContainer: {
-      width: spacing.SCALE_16,
-      height: spacing.SCALE_16,
+      height: moderateScale(24),
+      width: moderateScale(24),
       borderRadius: spacing.SCALE_128,
+      backgroundColor: colors.natural.natural0,
+      shadowOffset: { width: 0, height: spacing.SCALE_4 },
+      shadowOpacity: 0.25,
+      shadowRadius: spacing.SCALE_1,
+      elevation: spacing.SCALE_1,
     },
     isOnParent: {
-      height: spacing.CUSTOME_SCALE(28),
-      width: spacing.CUSTOME_SCALE(51),
-      padding: spacing.SCALE_4,
       backgroundColor: colors.tertiary.tertiary500,
       alignItems: 'flex-end',
     },
     isOffParent: {
-      width: spacing.CUSTOME_SCALE(51),
-      height: spacing.CUSTOME_SCALE(28),
-      padding: spacing.SCALE_4,
       backgroundColor: colors.natural.natural200,
       alignItems: 'flex-start',
     },
     isOnChild: {
-      height: spacing.CUSTOME_SCALE(22),
-      width: spacing.CUSTOME_SCALE(22),
-      backgroundColor: colors.natural.natural0,
       alignItems: 'flex-end',
-      shadowColor: colors.natural.natural1000,
-      shadowOffset: { width: 0, height: spacing.SCALE_4 },
-      shadowOpacity: 0.25,
-      shadowRadius: spacing.SCALE_1,
-      elevation: spacing.SCALE_1,
+      shadowColor: colors.natural.nchildContaineratural1000,
     },
     isOffChild: {
-      height: spacing.CUSTOME_SCALE(22),
-      width: spacing.CUSTOME_SCALE(22),
-      backgroundColor: colors.natural.natural0,
       alignItems: 'flex-start',
       shadowColor: colors.natural.natural1000,
-      shadowOffset: { width: 0, height: spacing.SCALE_4 },
-      shadowOpacity: 0.25,
-      shadowRadius: spacing.SCALE_1,
-      elevation: spacing.SCALE_1,
     },
   });
 
