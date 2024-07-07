254cb85a3b6a7daf40f8e579eb54f22ca5ed2a8d
Moeed Sarwar <moeed.c@uxbert.com>
2024-07-07 18:25:14 +0500

    fixed lint issues

--- app/src/components/templates/ipay-card-issue-bottomsheet/ipay-card-issue-bottomsheet.styles.tsx
+++ app/src/components/templates/ipay-card-issue-bottomsheet/ipay-card-issue-bottomsheet.styles.tsx
@@ -1,9 +1,9 @@
+import colors from "@app/styles/colors.const";
 import createStyleSheet from "@app/styles/scaled-sheet.styles";
 import { FONT_SIZE_12, FONT_SIZE_13 } from "@app/styles/typography.styles";
-import { mode } from "crypto-js";
 import { moderateScale } from "react-native-size-matters";
 
-const CardIssueStyle = (colors) => createStyleSheet({
+const CardIssueStyle = (themeColors: typeof colors) => createStyleSheet({
   margin: {
     paddingHorizontal: moderateScale(16)
   },
@@ -15,15 +15,15 @@ const CardIssueStyle = (colors) => createStyleSheet({
     gap: moderateScale(8)
   },
   titleColor: {
-    color: colors.primary.primary900,
+    color: themeColors.primary.primary900,
     marginVertical: moderateScale(8)
   },
   detailsStyle: {
-    color: colors.natural.natural500,
+    color: themeColors.natural.natural500,
     fontSize: FONT_SIZE_12
   },
   titleStyle: {
-    color: colors.natural.natural900,
+    color: themeColors.natural.natural900,
     fonstSize: FONT_SIZE_13
   },
   buttonContainer: {
