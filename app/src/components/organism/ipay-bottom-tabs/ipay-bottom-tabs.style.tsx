70444c0f51eee928504000f920469726450e70fd
mariauxbert <maria.c@uxbert.com>
2024-07-08 16:21:08 +0500

    fix: update accoeding to new format

--- app/src/components/organism/ipay-bottom-tabs/ipay-bottom-tabs.style.tsx
+++ app/src/components/organism/ipay-bottom-tabs/ipay-bottom-tabs.style.tsx
@@ -1,3 +1,4 @@
+import { constants } from '@app/components/atoms/ipay-text/constants.text';
 import createStyleSheet from '@app/styles/scaled-sheet.styles';
 import { SCALE_24, SCALE_48, SCALE_6 } from '@app/styles/spacing.const';
 import { FONT_SIZE_12 } from '@app/styles/typography.styles';
@@ -15,28 +16,30 @@ const bottomTabStyles = (theme: any) =>
       justifyContent: 'space-between',
       alignItems: 'center',
       paddingHorizontal: SCALE_24,
-      position: 'absoulte'
+      position: 'absoulte',
     },
     tabBar: {
       position: 'absolute',
       bottom: 0,
       left: 0,
       right: 0,
-      zIndex: 1001 // Ensure the tab bar has a higher zIndex
+      zIndex: 1001,
     },
     captionTextStyle: {
       fontSize: FONT_SIZE_12,
       fontWeight: '700',
-      marginTop: SCALE_6
+      marginTop: SCALE_6,
     },
     tabContainerStyle: {
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center',
     },
     buttonStyle: {
       flex: 1,
-      alignItems: 'center'
-    }
+      alignItems: 'center',
+    },
+    focusedText: { color: theme.primary.primary900, fontWeight: constants.FONT_WEIGHT_BOLD },
+    blurText: { color: theme.natural.natural500, fontWeight: constants.FONT_WEIGHT_NORMAL },
   });
 
 export default bottomTabStyles;
