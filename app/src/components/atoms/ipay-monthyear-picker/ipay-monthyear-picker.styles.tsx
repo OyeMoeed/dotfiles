c2303fff33d758e84d99f58dbcfce2af3c3e6bc3
mariauxbert <maria.c@uxbert.com>
2024-07-08 18:28:48 +0500

    fix: interface fix

--- app/src/components/atoms/ipay-monthyear-picker/ipay-monthyear-picker.styles.tsx
+++ app/src/components/atoms/ipay-monthyear-picker/ipay-monthyear-picker.styles.tsx
@@ -1,5 +1,6 @@
 import colors from '@app/styles/colors.const';
 import createStyleSheet from '@app/styles/scaled-sheet.styles';
+import { FONT_WEIGHT_BOLD } from '@app/styles/typography.styles';
 import { moderateScale } from 'react-native-size-matters';
 import { typography } from '../ipay-text/utilities/typography-helper.util';
 
@@ -13,13 +14,12 @@ const datePickerStyles = (themeColor: typeof colors) =>
       width: moderateScale(150),
       backgroundColor: 'transparent',
       height: moderateScale(160),
-      fontWeight: 'bold',
     },
     itemSize: {
       fontSize: typography.FONT_VARIANTS.TITLE3.FONT_SIZE,
       lineHeight: typography.FONT_VARIANTS.TITLE3.LINE_HEIGHT,
       letterSpacing: typography.FONT_VARIANTS.TITLE3.LETTER_SPACING,
-      fontWeight: 'bold',
+      fontWeight: FONT_WEIGHT_BOLD,
       color: themeColor.primary.primary500,
     },
   });
