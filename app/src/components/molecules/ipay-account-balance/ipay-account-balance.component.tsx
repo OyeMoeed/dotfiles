bfe44ac76ac2bf7cac0b7972e5d3eac7f1172577
JavedUxbert <154338382+JavedUxbert@users.noreply.github.com>
2024-07-10 02:50:00 +0500

    feat: design amount balance component on card renewal screen

---
+++ app/src/components/molecules/ipay-account-balance/ipay-account-balance.component.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { IPayButton } from '@app/components/molecules';
+
+import useTheme from '@app/styles/hooks/theme.hook';
+import useLocalization from '@app/localization/hooks/localization.hook';
+import { IPayCaption2Text, IPayIcon, IPaySubHeadlineText, IPayView } from '@app/components/atoms';
+import { buttonVariants } from '@app/utilities/enums.util';
+import icons from '@app/assets/icons';
+import ipayAccountBalanceStyles from './ipay-account-balance.style';
+import { IPayAccountBalanceProps } from './ipay-account-balance.interface';
+
+const IPayAccountBalance: React.FC<IPayAccountBalanceProps> = ({ balance, onPressTopup }) => {
+  const { colors } = useTheme();
+
+  const localizationText = useLocalization();
+
+  const styles = ipayAccountBalanceStyles(colors);
+
+  return (
+    <IPayView testID="account-balance-component" style={styles.container}>
+      <IPayView style={styles.textContainer}>
+        <IPayCaption2Text text={localizationText.HOME.ACCOUNT_BALANCE} style={styles.textColor} />
+        <IPayView style={styles.balanceContainer}>
+          <IPaySubHeadlineText testID="balance-text" style={styles.textColor} text={balance} />
+          <IPaySubHeadlineText style={styles.textColor} regular text={localizationText.COMMON.IPaySafeAreaView} />
+        </IPayView>
+      </IPayView>
+      <IPayButton
+        testID="topup-button"
+        onPress={onPressTopup}
+        btnType={buttonVariants.PRIMARY}
+        leftIcon={<IPayIcon icon={icons.add} size={18} color={colors.primary.primary500} />}
+        btnText={localizationText.COMMON.TOP_UP}
+        textColor={colors.primary.primary500}
+        btnStyle={styles.buttonStyle}
+      />
+    </IPayView>
+  );
+};
+
+export default IPayAccountBalance;
---
+++ app/src/components/molecules/ipay-account-balance/ipay-account-balance.style.tsx
@@ -0,0 +1,38 @@
+import createStyleSheet from '@app/styles/scaled-sheet.styles';
+import { moderateScale } from 'react-native-size-matters';
+
+const ipayAccountBalanceStyles = (colors: any) =>
+  createStyleSheet({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      backgroundColor: colors.natural.natural4,
+      borderRadius: 16,
+      borderWidth: 1,
+      borderColor: colors.natural.natural0,
+      paddingHorizontal: moderateScale(24),
+      paddingVertical: moderateScale(16),
+    },
+    textContainer: {
+      gap: moderateScale(2),
+    },
+    textColor: {
+      color: colors.natural.natural900,
+    },
+    balanceContainer: {
+      flexDirection: 'row',
+      gap: moderateScale(4),
+    },
+    buttonStyle: {
+      minHeight: moderateScale(34),
+      width: moderateScale(99),
+      height: 'auto',
+      borderWidth: 1,
+      borderColor: colors.primary.primary500,
+      borderRadius: moderateScale(12),
+      justifyContent: 'center',
+    },
+  });
+
+export default ipayAccountBalanceStyles;
