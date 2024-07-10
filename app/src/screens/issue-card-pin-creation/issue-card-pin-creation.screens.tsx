e6520609d1a742a8eb02721230c25d4ba0f1ebf5
mariauxbert <maria.c@uxbert.com>
2024-07-11 00:45:55 +0500

    fix: validation add

---
+++ app/src/screens/issue-card-pin-creation/issue-card-pin-creation.screens.tsx
@@ -0,0 +1,137 @@
+import constants from '@app/constants/constants';
+import useLocalization from '@app/localization/hooks/localization.hook';
+import useTheme from '@app/styles/hooks/theme.hook';
+import icons from '@assets/icons/index';
+import changeCardPinStyles from './issue-card-pin-creation.style';
+
+import { BulkLock } from '@app/assets/svgs';
+import { IPayIcon, IPayView } from '@app/components/atoms';
+import { IPayPageDescriptionText } from '@app/components/molecules';
+import { useToastContext } from '@app/components/molecules/ipay-toast/context/ipay-toast-context';
+import { IPayPasscode } from '@app/components/organism';
+import { forwardRef, useState } from 'react';
+import { ChangeCardPinProps, ChangeCardPinViewTypes } from './issue-card-pin-creation.interface';
+
+const IssueCardPinCreationScreen = forwardRef(({ onSuccess }: ChangeCardPinProps) => {
+  const { colors } = useTheme();
+  const styles = changeCardPinStyles(colors);
+  const localizationText = useLocalization();
+  const [passcodeError, setPasscodeError] = useState(false);
+  const [currentView, setCurrentView] = useState<ChangeCardPinViewTypes>(ChangeCardPinViewTypes.NewPin);
+  const [newPin, setNewPin] = useState<string>('');
+  const [clearPin, setClearPin] = useState<boolean>();
+
+  const getScreenTitle = (currentView: string) => {
+    switch (currentView) {
+      case ChangeCardPinViewTypes.NewPin:
+        return localizationText.CHANGE_PIN.NEW_PIN_CODE;
+      case ChangeCardPinViewTypes.ConfirmNewPin:
+        return localizationText.CHANGE_PIN.CONFIRM_NEW_PIN;
+      default:
+        return '';
+    }
+  };
+
+  const getScreenDescription = (currentView: string) => {
+    switch (currentView) {
+      case ChangeCardPinViewTypes.NewPin:
+        return localizationText.CHANGE_PIN.YOU_WILL_NEED_TO;
+      case ChangeCardPinViewTypes.ConfirmNewPin:
+        return localizationText.CHANGE_PIN.ENTER_PASS_AGAIN;
+      default:
+        return '';
+    }
+  };
+
+  const getErrorTitle = (currentView: string) => {
+    switch (currentView) {
+      case ChangeCardPinViewTypes.NewPin:
+        return localizationText.CHANGE_PIN.INVALID_PIN;
+      case ChangeCardPinViewTypes.ConfirmNewPin:
+        return localizationText.CHANGE_PIN.PIN_NOT_MATCHING;
+      default:
+        return '';
+    }
+  };
+
+  const getErrorDescription = (currentView: string) => {
+    switch (currentView) {
+      case ChangeCardPinViewTypes.NewPin:
+        return localizationText.CHANGE_PIN.OLD_PIN;
+      case ChangeCardPinViewTypes.ConfirmNewPin:
+        return localizationText.CHANGE_PIN.ENSURE_YOU_WRITE;
+      default:
+        return '';
+    }
+  };
+
+  const { showToast } = useToastContext();
+
+  const onVerifyPin = (enteredCode: string) => enteredCode === '1234'; //TODO: pincode hardcoded for now will be change later
+
+  const checkIfPinNotOldPin = (enteredCode: string) => enteredCode !== '1234';
+
+  const isPinMatched = (enteredCode: string) => enteredCode === newPin;
+
+  const onEnterPassCode = (enteredCode: string) => {
+    if (passcodeError) {
+      setPasscodeError(false);
+    }
+    if (enteredCode.length !== 4) return;
+
+    switch (currentView) {
+      case ChangeCardPinViewTypes.NewPin:
+        if (checkIfPinNotOldPin(enteredCode)) {
+          setNewPin(enteredCode);
+          setCurrentView(ChangeCardPinViewTypes.ConfirmNewPin);
+          setClearPin((prev) => !prev);
+        } else {
+          setPasscodeError(true);
+          renderToast();
+        }
+        break;
+      case ChangeCardPinViewTypes.ConfirmNewPin:
+        if (isPinMatched(enteredCode)) {
+          setClearPin((prev) => !prev);
+          onSuccess && onSuccess();
+        } else {
+          setPasscodeError(true);
+          renderToast();
+        }
+        break;
+      default:
+        return '';
+    }
+  };
+
+  const renderToast = () => {
+    showToast({
+      title: getErrorTitle(currentView),
+      subTitle: getErrorDescription(currentView),
+      containerStyle: styles.toast,
+      isShowRightIcon: false,
+      leftIcon: <IPayIcon icon={icons.warning} size={24} color={colors.natural.natural0} />,
+    });
+  };
+
+  return (
+    <IPayView style={styles.container}>
+      <IPayView style={styles.lockIconView}>
+        <BulkLock />
+      </IPayView>
+      <IPayView style={styles.headingView}>
+        <IPayPageDescriptionText heading={getScreenTitle(currentView)} text={getScreenDescription(currentView)} />
+      </IPayView>
+      <IPayView style={styles.pincodeViewContainer}>
+        <IPayPasscode
+          clearPin={clearPin}
+          passcodeError={passcodeError}
+          data={constants.DIALER_DATA}
+          onEnterPassCode={onEnterPassCode}
+        />
+      </IPayView>
+    </IPayView>
+  );
+});
+
+export default IssueCardPinCreationScreen;
