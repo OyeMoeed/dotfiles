4982da486b4954a768380cf89bb7f1adc2e6a46f
Moeed Sarwar <moeed.c@uxbert.com>
2024-07-11 02:45:39 +0500

    added bottomsheet

--- app/src/components/organism/ipay-send-money-form/ipay-send-money-form.component.tsx
+++ app/src/components/organism/ipay-send-money-form/ipay-send-money-form.component.tsx
@@ -1,46 +1,34 @@
-import icons from '@app/assets/icons';
-import {
-  IPayFootnoteText,
-  IPayIcon,
-  IPayImage,
-  IPayView,
-} from '@app/components/atoms';
-import {
-  IPayAmountInput,
-  IPayAnimatedTextInput,
-  IPayButton,
-  IPayList,
-  IPayTopUpBox,
-} from '@app/components/molecules';
-import useLocalization from '@app/localization/hooks/localization.hook';
-import useTheme from '@app/styles/hooks/theme.hook';
-import React, { useCallback, useRef, useState } from 'react';
-import { FlatList } from 'react-native';
-import IPaySendMoneyFormStyles from './ipay-send-money-form.styles';
-import { IPaySafeAreaView } from '@app/components/templates';
-import { IPayActionSheetProps } from '../ipay-actionsheet/ipay-actionsheet-interface';
-import IPayActionSheet from '../ipay-actionsheet/ipay-actionsheet.component';
-import images from '@app/assets/images';
-import { formatNumberWithCommas } from '@app/utilities/number-comma-helper.util';
+import icons from "@app/assets/icons";
+import images from "@app/assets/images";
+import { IPayIcon, IPayView, IPayImage, IPayPressable, IPayFlatlist } from "@app/components/atoms";
+import { IPayButton, IPayList, IPayAmountInput, IPayAnimatedTextInput, IPayTopUpBox } from "@app/components/molecules";
+import { IPaySafeAreaView } from "@app/components/templates";
+import useLocalization from "@app/localization/hooks/localization.hook";
+import { formatNumberWithCommas } from "@app/utilities/number-comma-helper.util";
+import { useRef, useState, useCallback } from "react";
+import { FlatList } from "react-native";
+import IPayActionSheet from "../ipay-actionsheet/ipay-actionsheet.component";
+import IPayBottomSheet from "../ipay-bottom-sheet/ipay-bottom-sheet.component";
+import IPaySendMoneyFormStyles from "./ipay-send-money-form.styles";
+import useTheme from "@app/styles/hooks/theme.hook";
 
 type FormProps = {
   id: number;
 };
 
-const IPaySendMoneyForm: React.FC = () => {
+const IPaySendMoneyForm = () => {
   const localizationText = useLocalization();
   const { colors } = useTheme();
   const styles = IPaySendMoneyFormStyles(colors);
+  const reasonBottomRef = useRef<any>(null);
   const actionSheetRef = useRef<any>(null);
-  const [amount, setAmount] = useState('')
+  const [amount, setAmount] = useState('');
+  const [formInstances, setFormInstances] = useState<FormProps[]>([{ id: 1 }]);
+  const [selectedItem, setSelectedItem] = useState<any>(null); // State to track selected item
 
-  const [formInstances, setFormInstances] = useState<FormProps[]>([
-    { id: 1 }  // Initial form instance
-  ]);
-
-  const handleAmountChange = () => {
-    setAmount(amount)
-  }
+  const handleAmountChange = (newAmount: string) => {
+    setAmount(newAmount);
+  };
 
   const addForm = () => {
     const newId = formInstances.length ? formInstances[formInstances.length - 1].id + 1 : 1;
@@ -50,6 +38,16 @@ const IPaySendMoneyForm: React.FC = () => {
   const removeForm = (id: number) => {
     setFormInstances(formInstances.filter(form => form.id !== id));
   };
+
+  const bottomSheetData = [
+    { id: 1, text: localizationText.SEND_MONEY_FORM.LIVING_EXPENSES },
+    { id: 2, text: localizationText.SEND_MONEY_FORM.ACCOMMODATION_FEES },
+    { id: 3, text: localizationText.SEND_MONEY_FORM.BILL_PAYMENT },
+    { id: 4, text: localizationText.SEND_MONEY_FORM.CAR_FINANCE_PAYMENT },
+    { id: 5, text: localizationText.SEND_MONEY_FORM.HOUSE_FINANCE_PAYMENT },
+    { id: 6, text: localizationText.SEND_MONEY_FORM.INSURANCE_PAYMENT },
+    { id: 7, text: localizationText.SEND_MONEY_FORM.RENT_PAYMENT }
+  ];
 
   const showActionSheet = useCallback((id: number) => {
     if (actionSheetRef.current) {
@@ -64,11 +62,11 @@ const IPaySendMoneyForm: React.FC = () => {
     }
   };
 
-  const actionSheetOptions: IPayActionSheetProps = {
-    title: localizationText.REMOVE_CONTACT,
+  const actionSheetOptions = {
+    title: localizationText.SEND_MONEY_FORM.REMOVE,
     showIcon: true,
     customImage: <IPayIcon icon={icons.TRASH} size={42} />,
-    message: localizationText.REMOVE_CONTACT_SUB,
+    message: localizationText.SEND_MONEY_FORM.REMOVE_DETAIL,
     options: [localizationText.PROFILE.REMOVE, localizationText.COMMON.CANCEL],
     cancelButtonIndex: 1,
     showCancel: true,
@@ -76,15 +74,42 @@ const IPaySendMoneyForm: React.FC = () => {
     onPress: handleActionSheetPress
   };
 
-  const renderIcon = () => {
-  }
+  const renderItemList = () => (
+    <IPayFlatlist
+      data={bottomSheetData}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <IPayView style={styles.container}>
+          <IPayList
+            textStyle={styles.titleStyle}
+            title={item.text}
+            isShowIcon={selectedItem && selectedItem.id === item.id}
+            icon={selectedItem && selectedItem.id === item.id && (
+              <IPayIcon icon={icons.tick_mark_default} size={20} color={colors.primary.primary500} />
+            )}
+            onPress={() => {
+              setSelectedItem(item); // Set selected item
+            }}
+          />
+        </IPayView>
+      )}
+    />
+  );
+
+  const openReason = () => {
+    reasonBottomRef.current.present();
+  };
+
+  const closeReason = () => {
+    reasonBottomRef.current.close();
+  };
 
   const renderItem = ({ item }) => (
     <IPayView key={item.id} style={styles.gradientView}>
       <IPayView>
         <IPayList
           textStyle={styles.titleText}
-          title={localizationText.RECIPIENT}
+          title={localizationText.SEND_MONEY_FORM.RECIPIENT}
           subTextStyle={styles.subtitleText}
           isShowSubTitle
           subTitle={localizationText.ahmed_mohamed}
@@ -96,11 +121,6 @@ const IPaySendMoneyForm: React.FC = () => {
         />
       </IPayView>
       <IPayView style={styles.inputContainer}>
-        <IPayFootnoteText
-          text={localizationText.enter_amount}
-          regular
-          color={colors.natural.natural700}
-        />
         <IPayAmountInput
           inputStyles={styles.inputText}
           currencyStyle={styles.currencyStyle}
@@ -109,12 +129,16 @@ const IPaySendMoneyForm: React.FC = () => {
           editable={true}
         />
       </IPayView>
-      <IPayAnimatedTextInput
-        containerStyle={styles.inputField}
-        label={localizationText.TRANSACTION_HISTORY.TRANSFER_REASON}
-        showRightIcon
-        customIcon={<IPayIcon icon={icons.arrow_circle_down} size={20} color={colors.primary.primary500} />}
-      />
+      <IPayPressable onPress={openReason}>
+        <IPayAnimatedTextInput
+          containerStyle={styles.inputField}
+          label={localizationText.TRANSACTION_HISTORY.TRANSFER_REASON}
+          value={selectedItem ? selectedItem.text : ''}
+          editable={false}
+          showRightIcon
+          customIcon={<IPayIcon icon={icons.arrow_circle_down} size={20} color={colors.primary.primary500} />}
+        />
+      </IPayPressable>
       <IPayAnimatedTextInput
         containerStyle={styles.inputField}
         label={localizationText.TRANSACTION_HISTORY.NOTE}
@@ -132,6 +156,7 @@ const IPaySendMoneyForm: React.FC = () => {
       </IPayView>
     </IPayView>
   );
+
   const balance = formatNumberWithCommas('5200.40');
 
   return (
@@ -142,9 +167,8 @@ const IPaySendMoneyForm: React.FC = () => {
         isShowRemaining
         isShowProgressBar
         currentBalance={balance}
-        monthlyRemainingOutgoingBalance= {balance}
+        monthlyRemainingOutgoingBalance={balance}
       />
-
       <FlatList
         data={formInstances}
         renderItem={renderItem}
@@ -155,7 +179,7 @@ const IPaySendMoneyForm: React.FC = () => {
             btnType="link-button"
             btnStyle={styles.chipContainer}
             textColor={colors.secondary.secondary800}
-            btnText={localizationText.ADD_MORE_RECIPIENTS}
+            btnText={localizationText.SEND_MONEY_FORM.ADD}
             hasLeftIcon
             leftIcon={<IPayIcon icon={icons.add} size={14} color={colors.secondary.secondary800} />}
             onPress={addForm}
@@ -163,11 +187,26 @@ const IPaySendMoneyForm: React.FC = () => {
         )}
       />
       <IPayActionSheet ref={actionSheetRef} {...actionSheetOptions} />
+      <IPayBottomSheet
+        heading={localizationText.TRANSACTION_HISTORY.TRANSACTION_DETAILS}
+        onCloseBottomSheet={closeReason}
+        customSnapPoint={['20%', '75%']}
+        ref={reasonBottomRef}
+        simpleHeader
+        simpleBar
+        cancelBnt
+        doneBtn
+        bold
+      >
+        {renderItemList()}
+      </IPayBottomSheet>
     </IPaySafeAreaView>
   );
 };
 
 export default IPaySendMoneyForm;
+
+
 
 
 
