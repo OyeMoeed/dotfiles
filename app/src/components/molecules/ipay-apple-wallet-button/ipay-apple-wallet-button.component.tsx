497540c2376c14047c90c3592b5775d4433224bf
mariauxbert <maria.c@uxbert.com>
2024-07-10 05:26:47 +0500

    feat: arabic translation

--- app/src/components/molecules/ipay-apple-wallet-button/ipay-apple-wallet-button.component.tsx
+++ app/src/components/molecules/ipay-apple-wallet-button/ipay-apple-wallet-button.component.tsx
@@ -4,18 +4,19 @@ import { navigate } from '@app/navigation/navigation-service.navigation';
 import screenNames from '@app/navigation/screen-names.navigation';
 import useTheme from '@app/styles/hooks/theme.hook';
 import React from 'react';
+import IPayAppleWalletButtonProps from './ipay-apple-wallet-button.interface';
 import IPayAppleWalletButtonStyles from './ipay-apple-wallet-button.styles';
 
-const IPayAppleWalletButton: React.FC = () => {
+const IPayAppleWalletButton: React.FC<IPayAppleWalletButtonProps> = (testID) => {
   const { colors } = useTheme();
   const styles = IPayAppleWalletButtonStyles(colors);
-  
+
   const handleAddToAppleWallet = () => {
     navigate(screenNames.WALLET);
   };
 
   return (
-    <IPayPressable onPress={handleAddToAppleWallet}>
+    <IPayPressable testID={`${testID}-apple-wallet-button`} onPress={handleAddToAppleWallet}>
       <IPayImage image={images.addAppleWallet} style={styles.imageStyles} />
     </IPayPressable>
   );
