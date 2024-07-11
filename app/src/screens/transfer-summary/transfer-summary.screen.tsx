7858074d5ab72d9f915cd81c7460a2cba2c553f1
Moeed Sarwar <moeed.c@uxbert.com>
2024-07-11 13:18:47 +0500

    fixed feed back

--- app/src/screens/transfer-summary/transfer-summary.screen.tsx
+++ app/src/screens/transfer-summary/transfer-summary.screen.tsx
@@ -1,68 +1,110 @@
-import icons from "@app/assets/icons";
-import { IPayView, IPayFlatlist, IPayFootnoteText, IPayIcon, IPayPressable, IPayImage } from "@app/components/atoms";
-import { IPaySafeAreaView } from "@app/components/templates";
-import useLocalization from "@app/localization/hooks/localization.hook";
-import useTheme from "@app/styles/hooks/theme.hook";
-import { scaleSize } from "@app/styles/mixins";
-import TransferScreenStyle from "./transfer-summary.styles";
-import { IPayButton, IPayChip, IPayHeader } from "@app/components/molecules";
-import images from "@app/assets/images";
+import icons from '@app/assets/icons';
+import images from '@app/assets/images';
+import { IPayFlatlist, IPayFootnoteText, IPayIcon, IPayImage, IPayPressable, IPayView } from '@app/components/atoms';
+import { IPayButton, IPayChip, IPayHeader } from '@app/components/molecules';
+import { IPaySafeAreaView } from '@app/components/templates';
+import useLocalization from '@app/localization/hooks/localization.hook';
+import useTheme from '@app/styles/hooks/theme.hook';
+import { scaleSize } from '@app/styles/mixins';
+import React from 'react';
+import TransferScreenStyle from './transfer-summary.styles';
 
-const TransferScreenSummary: React.FC = () => {
+const TransferSummaryScreen: React.FC = () => {
   const { colors } = useTheme();
   const localizationText = useLocalization();
   const styles = TransferScreenStyle(colors);
 
   const alinmaDetails = [
     {
-      id: '1', label: localizationText.TRANSFER_SUMMARY.TRANSFER_TO, value: localizationText.TRANSFER_SUMMARY.ADAM_AHMAD, leftIcon: images.logoTab, isAlinma: true
+      id: '1',
+      label: localizationText.TRANSFER_SUMMARY.TRANSFER_TO,
+      value: localizationText.TRANSFER_SUMMARY.ADAM_AHMAD,
+      leftIcon: images.logoTab,
+      isAlinma: true,
     },
     { id: '2', label: localizationText.TRANSFER_SUMMARY.AMOUNT, value: localizationText.TRANSFER_SUMMARY.MONEY },
-    { id: '3', label: localizationText.TRANSFER_SUMMARY.REASON, value: localizationText.TRANSFER_SUMMARY.REASON_TRANSFER },
+    {
+      id: '3',
+      label: localizationText.TRANSFER_SUMMARY.REASON,
+      value: localizationText.TRANSFER_SUMMARY.REASON_TRANSFER,
+    },
     { id: '4', label: localizationText.TRANSFER_SUMMARY.NOTE, value: localizationText.TRANSFER_SUMMARY.NOTE_DETAIL },
   ];
 
   const nonAlinmaDetails = [
     {
-      id: '1', label: localizationText.TRANSFER_SUMMARY.TRANSFER_TO, value: localizationText.TRANSFER_SUMMARY.ERSA_ALTURK, leftIcon: icons.user_square, color: colors.primary.primary900, isAlinma: false
+      id: '1',
+      label: localizationText.TRANSFER_SUMMARY.TRANSFER_TO,
+      value: localizationText.TRANSFER_SUMMARY.ERSA_ALTURK,
+      leftIcon: icons.user_square,
+      color: colors.primary.primary900,
+      isAlinma: false,
     },
     { id: '2', label: localizationText.TRANSFER_SUMMARY.AMOUNT, value: localizationText.TRANSFER_SUMMARY.AMOUNT_2 },
-    { id: '3', label: localizationText.TRANSFER_SUMMARY.REASON, value: localizationText.TRANSFER_SUMMARY.REASON_TRANSFER },
+    {
+      id: '3',
+      label: localizationText.TRANSFER_SUMMARY.REASON,
+      value: localizationText.TRANSFER_SUMMARY.REASON_TRANSFER,
+    },
   ];
 
-  const renderWalletPayItem = ({ item }) => (
-    <IPayView style={styles.listContainer}>
-      <IPayView style={styles.walletListBackground}>
-        <IPayView style={styles.iconLabel}>
-          {item.leftIcon && item.isAlinma ? (
+  const renderWalletPayItem = ({ item }) => {
+    const renderLeftIcon = () => {
+      if (item.leftIcon) {
+        if (item.isAlinma) {
+          return (
             <IPayView style={styles.leftIcon}>
               <IPayImage image={item.leftIcon} style={styles.alinmaLogo} resizeMode="contain" />
             </IPayView>
-          ) : item.leftIcon ? (
-            <IPayPressable style={styles.appleIcon} onPress={item.onPress}>
-              <IPayIcon icon={item.leftIcon} style={styles.appleIcon} color={item.color} size={scaleSize(18)} />
-            </IPayPressable>
-          ) : null}
-          <IPayFootnoteText text={item.label} style={styles.label} />
-        </IPayView>
-        <IPayView style={styles.listDetails}>
-          <IPayFootnoteText text={item.value} style={styles.detailsText} />
-          {item.icon && (
-            <IPayPressable style={styles.appleIcon} onPress={item.onPress}>
-              <IPayIcon icon={item.icon} style={styles.appleIcon} color={item.color} size={scaleSize(18)} />
-            </IPayPressable>
-          )}
+          );
+        }
+        return (
+          <IPayPressable style={styles.appleIcon} onPress={item.onPress}>
+            <IPayIcon icon={item.leftIcon} style={styles.appleIcon} color={item.color} size={scaleSize(18)} />
+          </IPayPressable>
+        );
+      }
+      return null;
+    };
+
+    return (
+      <IPayView style={styles.listContainer}>
+        <IPayView style={styles.walletListBackground}>
+          <IPayView style={styles.iconLabel}>
+            {renderLeftIcon()}
+            <IPayFootnoteText text={item.label} style={styles.label} />
+          </IPayView>
+          <IPayView style={styles.listDetails}>
+            <IPayFootnoteText text={item.value} style={styles.detailsText} />
+            {item.icon && (
+              <IPayPressable style={styles.appleIcon} onPress={item.onPress}>
+                <IPayIcon icon={item.icon} style={styles.appleIcon} color={item.color} size={scaleSize(18)} />
+              </IPayPressable>
+            )}
+          </IPayView>
         </IPayView>
       </IPayView>
-    </IPayView>
-  );
+    );
+  };
+  const renderNonAlinmaPayItem = ({ item, index }) => {
+    const isFirstItem = index === 0;
 
-  const renderNonAlinmaPayItem = ({ item, index }) => (
-    <>
-      {index === 0 && (<IPayView style={styles.chipContainer}>< IPayChip containerStyle={styles.chipColors} icon={<IPayIcon icon={icons.SHEILD} color={colors.secondary.secondary500} size={18} />} textValue={localizationText.TRANSFER_SUMMARY.CHIP_TITLE} headingStyles={styles.chipColors} /></IPayView>)}
-      {renderWalletPayItem({ item })}
-    </>
-  );
+    return (
+      <IPayView key={item.id}>
+        {isFirstItem && (
+          <IPayView style={styles.chipContainer}>
+            <IPayChip
+              containerStyle={styles.chipColors}
+              icon={<IPayIcon icon={icons.SHEILD} color={colors.secondary.secondary500} size={18} />}
+              textValue={localizationText.TRANSFER_SUMMARY.CHIP_TITLE}
+              headingStyles={styles.chipColors}
+            />
+          </IPayView>
+        )}
+        {renderWalletPayItem({ item })}
+      </IPayView>
+    );
+  };
 
   return (
     <IPaySafeAreaView linearGradientColors={colors.appGradient.gradientPrimary50}>
@@ -87,13 +129,17 @@ const TransferScreenSummary: React.FC = () => {
           </IPayView>
         </IPayView>
         <IPayView>
-          <IPayButton btnType="primary" btnIconsDisabled btnText={localizationText.COMMON.CONFIRM} btnColor={colors.primary.primary500} medium />
+          <IPayButton
+            btnType="primary"
+            btnIconsDisabled
+            btnText={localizationText.COMMON.CONFIRM}
+            btnColor={colors.primary.primary500}
+            medium
+          />
         </IPayView>
       </IPayView>
     </IPaySafeAreaView>
   );
 };
 
-export default TransferScreenSummary;
-
-
+export default TransferSummaryScreen;
