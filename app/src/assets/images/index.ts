efcd44ca455c59bf4f2d5d72009b49464dfcfd69
mariauxbert <maria.c@uxbert.com>
2024-07-11 17:52:07 +0500

    Merge remote-tracking branch 'origin/feature/ipay-163-card-management' into ipay-163-card-management/task/ipay-1741-card-issuace-confirmation-details

--- app/src/assets/images/index.ts
+++ app/src/assets/images/index.ts
@@ -43,6 +43,7 @@ const images = {
   platinumCard: require('./platinum-card.png'),
   signatueCard: require('./signature-card.png'),
   master: require('./master.png'),
+  visa: require('./visa.png'),
   dateCard: require('./dateCard.png'),
   cvvCard: require('./cvvCard.png'),
 };
