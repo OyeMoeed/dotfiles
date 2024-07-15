520e1587c73757df79b01fa0a001724c93e18ce9
mariauxbert <165198119+mariauxbert@users.noreply.github.com>
2024-07-16 02:05:42 +0500

    Update main.stack.tsx

--- app/src/navigation/stacks/main/main.stack.tsx
+++ app/src/navigation/stacks/main/main.stack.tsx
@@ -54,6 +54,7 @@ const MainStackNavigator = () => {
           <MainStack.Screen name={screenNames.POINTS_REDEMPTIONS} component={PointsRedemptionsScreen} />
           <MainStack.Screen name={screenNames.CARD_ISSUE_CONFIRMATION} component={CardIssuanceConfirmationScreen} />
           <MainStack.Screen name={screenNames.WALLET_TRANSFER} component={WalletToWalletTransfer} />
+
           <MainStack.Screen
             name={screenNames.POINTS_REDEMPTIONS_CONFIRMATION}
             component={PointsRedemptionConfirmation}
