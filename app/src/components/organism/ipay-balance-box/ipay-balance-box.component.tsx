3ca2fb1a7ba41fb44b9c933daa345362e843d5a7
mariauxbert <165198119+mariauxbert@users.noreply.github.com>
2024-07-16 02:04:59 +0500

    Update ipay-balance-box.component.tsx

--- app/src/components/organism/ipay-balance-box/ipay-balance-box.component.tsx
+++ app/src/components/organism/ipay-balance-box/ipay-balance-box.component.tsx
@@ -27,16 +27,17 @@ import { CarouselItem, IPayBalanceBoxProps } from './ipay-balance-box.interface'
 import genratedStyles from './ipay-balance-box.styles';
 
 const IPayBalanceBox: React.FC = forwardRef<{}, IPayBalanceBoxProps>(
-  ({
-    testID,
-    balance = '5,200.40',
-    totalBalance = '20,000',
-    hideBalance,
-    walletInfoPress,
-    topUpPress,
-    quickAction,
-    setBoxHeight,
-  }) => {
+  (
+    { testID, 
+      balance = '5,200.40', 
+      totalBalance = '20,000', 
+      hideBalance, 
+      walletInfoPress, 
+      topUpPress, 
+      quickAction,
+      setBoxHeight,
+    }
+  ) => {
     const carouselData = useCarouselData();
     const buttonTypes = constants.BUTTON_TYPES;
     const { colors } = useTheme();
@@ -87,13 +88,13 @@ const IPayBalanceBox: React.FC = forwardRef<{}, IPayBalanceBoxProps>(
     };
 
     return (
-      <IPayView
-        testID={`${testID}-balance-box`}
-        style={styles.container}
-        onLayout={({ nativeEvent }) => {
-          const { height } = nativeEvent.layout;
-          setBoxHeight && setBoxHeight(height);
-        }}
+      <IPayView 
+      testID={`${testID}-balance-box`} 
+      style={styles.container}
+      onLayout={({ nativeEvent }) => {
+        const { height } = nativeEvent.layout;
+        setBoxHeight && setBoxHeight(height);
+      }}
       >
         {/* Card Text */}
         <IPayView style={[styles.commonContainer]}>
