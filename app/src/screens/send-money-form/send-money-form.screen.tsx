4fb7006a6ce4934ef391d7d8d1f127540dba1192
mariauxbert <maria.c@uxbert.com>
2024-07-16 04:10:25 +0500

    feat: send money form screen

--- app/src/screens/send-money-form/send-money-form.screen.tsx
+++ app/src/screens/send-money-form/send-money-form.screen.tsx
@@ -1,18 +1,18 @@
+import { IPaySendMoneyForm } from '@app/components/organism';
 import { IPaySafeAreaView } from '@app/components/templates';
 import useLocalization from '@app/localization/hooks/localization.hook';
 import useTheme from '@app/styles/hooks/theme.hook';
-import { IPayText } from '@components/atoms';
-import React from 'react';
+import React, { useState } from 'react';
 import sendMoneyFormStyles from './send-money-form.styles';
-
 
 const SendMoneyFormScreen: React.FC = () => {
   const { colors } = useTheme();
   const styles = sendMoneyFormStyles(colors);
   const localizationText = useLocalization();
+  const [amount, setAmount] = useState('');
   return (
     <IPaySafeAreaView style={styles.container}>
-      <IPayText>{localizationText.marketplace}</IPayText>
+      <IPaySendMoneyForm amount={amount} setAmount={setAmount} />
     </IPaySafeAreaView>
   );
 };
