07ef98a86676b891007563b554874588d3451020
mariauxbert <maria.c@uxbert.com>
2024-07-19 04:29:47 +0500

    feat: edit / delete beneficiary

--- app/src/components/organism/ipay-activate-beneficiary/ipay-activate-beneficiary.component.tsx
+++ app/src/components/organism/ipay-activate-beneficiary/ipay-activate-beneficiary.component.tsx
@@ -7,7 +7,31 @@ import activateBeneficiaryStyles from './ipay-activate-beneficiary.styles';
 const IPayActivateBeneficiary: React.FC<IPayActivateBeneficiaryProps> = ({ testID }) => {
   const { colors } = useTheme();
   const styles = activateBeneficiaryStyles(colors);
-  return <IPayView testID={`${testID}-card-details`} style={styles.container}></IPayView>;
+  const localizationText = useLocalization();
+  return (
+    <IPayView testID={`${testID}-activate-beneficiary`} style={styles.container}>
+      <IPayCaption1Text
+        text={localizationText.ACTIVATE_BENEFICIARY.CHOOSE_ACTIVATION_OPTION}
+        style={styles.descriptionStyles}
+      />
+      <IPayButton
+        btnType="primary"
+        btnText={localizationText.ACTIVATE_BENEFICIARY.RECEIVE_A_CALL_TO_ACTIVATE}
+        large
+        btnStyle={styles.callBtn}
+        leftIcon={<IPayIcon icon={icons.call_calling} size={20} color={colors.natural.natural0} />}
+        onPress={handleReceiveCall}
+      />
+      <IPayButton
+        btnType="outline"
+        btnText={localizationText.ACTIVATE_BENEFICIARY.CALL_ALINMA_TO_ACTIVATE}
+        large
+        leftIcon={<IPayIcon icon={icons.call_calling} size={20} color={colors.primary.primary500} />}
+        btnStyle={styles.callBtn}
+        onPress={handleCallAlinma}
+      />
+    </IPayView>
+  );
 };
 
 export default IPayActivateBeneficiary;
