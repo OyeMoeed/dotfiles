8dc422464562c0d32a7c43f7b3eb42324ab37ac8
Moeed Sarwar <moeed.c@uxbert.com>
2024-07-19 21:07:55 +0500

    refactor: fix non alinma logo style

--- app/src/navigation/app-navigator.navigation.tsx
+++ app/src/navigation/app-navigator.navigation.tsx
@@ -19,8 +19,7 @@ const MainNavigation: React.FC = () => {
   const { localizationFlag, appData, isAuthorized } = useTypedSelector((state) => ({
     localizationFlag: state.localizationReducer.localizationFlag,
     appData: state.appDataReducer.appData,
-    isAuthorized: state.auth.isAuthorized
-
+    isAuthorized: true,
   }));
   const isLanguageSheetVisible = useTypedSelector((state) => state.languageReducer.isLanguageSheetVisible);
   const { i18n } = useTranslation();
