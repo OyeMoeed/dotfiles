328876c458126bb0e9483c43ce931af7a769aaa6
Moustafa Mohamed <maamohamed@alinma.com>
2024-07-25 10:13:27 +0300

    feat: add faq and change lang backend integration

--- app/src/network/services/core/core.urls.ts
+++ app/src/network/services/core/core.urls.ts
@@ -1,10 +1,10 @@
 const CORE_URLS = {
   PREPARE_FORGET_PASSCODE: 'core/v1/user/forget-password/prepare',
   FORGET_PASSCODE: 'core/v1/user/forget-password/confirm',
-  CHANGE_LANGUAGE: 'core/v1/wallet/10587981/update',
   APP_CONFIGURATIONS: 'core/v1/appConfigurations',
   GET_WALLET_INFO: 'core/v1/wallet',
   SET_PASSCODE: 'core-management/v1/user/register',
+  FAQ: 'core-management/v1/faq',
   GET_OFFERS: (walletNumber: string) => `core/v1/${walletNumber}/offers`,
   GET_TOP_UP_CARDS: (walletNumber: string) => `core/v1/${walletNumber}/topup-cards`,
   GET_TRANSACTIONS: (walletNumber: string) => `core/v1/${walletNumber}/transaction`,
@@ -16,10 +16,11 @@ const CORE_URLS = {
     `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=GOOGLE_MAPS_API_KEY`,
   GET_WALLET: (walletNumber: string) => `core-management/v1/wallet/${walletNumber}`,
   GET_HOME_TRANSACTIONS: (walletNumber: string, maxRecords: string, offset: string) => `core-management/v1/${walletNumber}/transaction?max-records=${maxRecords}&offset=${offset}`,
-  GET_HOME_OFFERS: (walletNumber: string, isHome: string) => `core-management/v1/${walletNumber}/offers?home=${isHome}`,
+  GET_HOME_OFFERS: (walletNumber?: string, isHome?: string) => `core-management/v1/${walletNumber}/offers?home=${isHome}`,
   PREPARE_ID_RENEWAL: (walletNumber?: string) => `core-management/v1/wallet/${walletNumber}/renewId/prepare`,
   CONFIRM_ID_RENEWAL: (walletNumber?: string) => `core-management/v1/wallet/${walletNumber}/renewId/confirm`,
   DEVICE_DELINK: (walletNumber?: string) => `core-management/v1/wallet/${walletNumber}/delink-device`,
+  CHANGE_LANGUAGE: (walletNumber?: string) => `core-management/v1/wallet/${walletNumber}/update`,
 };
 
 export default CORE_URLS;
