ec14ff798a1dc33c1cc72a758bf16c4fcef8c576
Moeed Sarwar <moeed.c@uxbert.com>
2024-07-29 12:23:28 +0500

    fix: fixed advised feedback

--- app/src/screens/money-request-summary/money-request-summary.screen.tsx
+++ app/src/screens/money-request-summary/money-request-summary.screen.tsx
@@ -19,7 +19,7 @@ import useTheme from '@app/styles/hooks/theme.hook';
 import { States } from '@app/utilities/enums.util';
 import { formatNumberWithCommas } from '@app/utilities/number-helper.util';
 import { useRoute } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { PayData } from './money-request-summary.interface';
 import moneyRequestStyles from './money-request-summary.styles';
 
@@ -38,7 +38,7 @@ const MoneyRequestSummaryScreen: React.FC = () => {
   const monthlyRemaining = parseFloat(monthlyRemainingOutgoingAmount);
   const updatedTopUpAmount = parseFloat(topUpAmount.replace(/,/g, ''));
 
-  const determineChipValue = () => {
+  const determineChipValue = useCallback(() => {
     if (monthlyRemaining === 0) {
       return localizationText.REQUEST_SUMMARY.NO_REMAINING_AMOUNT;
     }
@@ -46,62 +46,69 @@ const MoneyRequestSummaryScreen: React.FC = () => {
       return localizationText.REQUEST_SUMMARY.INSUFFICIENT_BALANCE;
     }
     return '';
-  };
+  }, [monthlyRemaining, updatedTopUpAmount, localizationText]);
 
   useEffect(() => {
     setChipValue(determineChipValue());
-  }, [monthlyRemaining, updatedTopUpAmount, localizationText]);
+  }, [determineChipValue]);
 
-  const renderChip = () =>
-    chipValue ? (
-      <IPayChip
-        textValue={chipValue}
-        variant={States.WARNING}
-        isShowIcon
-        containerStyle={styles.chipContainer}
-        icon={
-          <IPayIcon
-            icon={chipValue === localizationText.TOP_UP.LIMIT_REACHED ? icons.warning : icons.shield_cross}
-            color={colors.critical.critical800}
-            size={16}
-          />
-        }
-      />
-    ) : (
-      <IPayList
-        title={localizationText.REQUEST_SUMMARY.AMOUNT}
-        rightText=<IPaySubHeadlineText
-          color={colors.primary.primary800}
-          regular
-          text={`${topUpAmount} ${localizationText.COMMON.SAR}`}
+  const renderChip = useMemo(
+    () =>
+      chipValue ? (
+        <IPayChip
+          textValue={chipValue}
+          variant={States.WARNING}
+          isShowIcon
+          containerStyle={styles.chipContainer}
+          icon={
+            <IPayIcon
+              icon={chipValue === localizationText.TOP_UP.LIMIT_REACHED ? icons.warning : icons.shield_cross}
+              color={colors.critical.critical800}
+              size={16}
+            />
+          }
         />
-      />
-    );
+      ) : (
+        <IPayList
+          title={localizationText.REQUEST_SUMMARY.AMOUNT}
+          rightText={
+            <IPaySubHeadlineText
+              color={colors.primary.primary800}
+              regular
+              text={`${topUpAmount} ${localizationText.COMMON.SAR}`}
+            />
+          }
+        />
+      ),
+    [chipValue, localizationText, topUpAmount, colors, icons],
+  );
 
-  const renderPayItem = ({ item }: { item: PayData }) => {
-    const { detailsText, leftIcon, label } = item;
-    return (
-      <IPayView style={styles.listContainer}>
-        <IPayView style={styles.listView}>
-          <IPayView style={styles.iconLabel}>
-            {leftIcon && (
-              <IPayView style={styles.leftIcon}>
-                <IPayImage image={images.alinmaP} style={styles.leftIconCard} resizeMode="contain" />
-              </IPayView>
-            )}
-            <IPayFootnoteText color={colors.natural.natural900} text={label} />
-          </IPayView>
-          <IPayView style={styles.listDetails}>
-            {detailsText ? (
-              <IPayFootnoteText text={detailsText} style={styles.detailsText} />
-            ) : (
-              <IPayFootnoteText text={`${topUpAmount} ${localizationText.COMMON.SAR}`} style={styles.detailsText} />
-            )}
+  const renderPayItem = useMemo(() => {
+    return ({ item }: { item: PayData }) => {
+      const { detailsText, leftIcon, label } = item;
+      return (
+        <IPayView style={styles.listContainer}>
+          <IPayView style={styles.listView}>
+            <IPayView style={styles.iconLabel}>
+              {leftIcon && (
+                <IPayView style={styles.leftIcon}>
+                  <IPayImage image={images.alinmaP} style={styles.leftIconCard} resizeMode="contain" />
+                </IPayView>
+              )}
+              <IPayFootnoteText color={colors.natural.natural900} text={label} />
+            </IPayView>
+            <IPayView style={styles.listDetails}>
+              {detailsText ? (
+                <IPayFootnoteText text={detailsText} style={styles.detailsText} />
+              ) : (
+                <IPayFootnoteText text={`${topUpAmount} ${localizationText.COMMON.SAR}`} style={styles.detailsText} />
+              )}
+            </IPayView>
           </IPayView>
         </IPayView>
-      </IPayView>
-    );
-  };
+      );
+    };
+  }, [topUpAmount, localizationText, colors, images]);
 
   return (
     <IPaySafeAreaView>
@@ -130,7 +137,7 @@ const MoneyRequestSummaryScreen: React.FC = () => {
           </IPayView>
         </IPayView>
         <IPayLinearGradientView style={styles.gradientBg}>
-          {renderChip()}
+          {renderChip}
           <IPayButton
             btnType="primary"
             medium
