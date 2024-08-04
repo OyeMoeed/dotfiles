5295493be0b09519ef30ebf23a4379159d392a68
HassanRaza <hassan.c@uxbert.com>
2024-08-04 19:55:04 +0500

    fix: language change not working in mock response fixed

--- app/src/network/services/core/change-language/change-language.mock.ts
+++ app/src/network/services/core/change-language/change-language.mock.ts
@@ -1,26 +1,23 @@
-import { ChangeLanguageMockProps } from './change-language.interface';
+import { ChangeLanguageDataProps } from './change-language.interface';
 
-const changeLanguageMock: ChangeLanguageMockProps = {
-  data: {
-    status: {
-      code: 'I000000',
-      type: 'SUCCESS',
-      desc: 'retail.msg.default.success',
-      sessionReference: 'SSPAYC0986bbcbbbb84505a109e2c90c36aeaa',
-      requestReference: '04750492912245578553',
-    },
-    response: {
-      walletNumber: '10587981',
-      walletStatus: 'A',
-      walletTier: 'G',
-      availableBalance: '1770.92',
-      currentBalance: '1775.92',
-      dormant: false,
-      idExpired: false,
-    },
-    successfulResponse: true,
+const changeLanguageMock: ChangeLanguageDataProps = {
+  status: {
+    code: 'I000000',
+    type: 'SUCCESS',
+    desc: 'retail.msg.default.success',
+    sessionReference: 'SSPAYC0986bbcbbbb84505a109e2c90c36aeaa',
+    requestReference: '04750492912245578553',
   },
-  ok: true,
+  response: {
+    walletNumber: '10587981',
+    walletStatus: 'A',
+    walletTier: 'G',
+    availableBalance: '1770.92',
+    currentBalance: '1775.92',
+    dormant: false,
+    idExpired: false,
+  },
+  successfulResponse: true,
 };
 
 export default changeLanguageMock;
