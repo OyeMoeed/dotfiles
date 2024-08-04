5960c83ff26208e104d42ad757ccebee65e6737d
Habib <habib.c@uxbert.com>
2024-08-04 23:07:05 +0500

    feat: add filters for order history

--- app/src/screens/order-history/all-orders.style.tsx
+++ app/src/screens/order-history/all-orders.style.tsx
@@ -1,7 +1,8 @@
 import createStyleSheet from '@app/styles/scaled-sheet.styles';
-import { moderateScale } from 'react-native-size-matters';
+import themeColors from '@app/styles/theming/theme-colors';
+import { moderateScale, verticalScale } from 'react-native-size-matters';
 
-const allOrdersStyles = () =>
+const allOrdersStyles = (colors: typeof themeColors) =>
   createStyleSheet({
     container: {
       justifyContent: 'center',
@@ -9,6 +10,18 @@ const allOrdersStyles = () =>
       flex: 1,
       marginHorizontal: moderateScale(22, 0.3),
       marginVertical: moderateScale(22),
+    },
+    filterWrapper: {
+      height: verticalScale(20),
+      marginTop: moderateScale(24),
+    },
+    chipContainer: {
+      marginLeft: moderateScale(10),
+      backgroundColor: colors.secondary.secondary100,
+    },
+    chipHeading: {
+      gap: moderateScale(10),
+      color: colors.secondary.secondary500,
     },
   });
 
