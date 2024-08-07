1648f9287103e1f8a6ba8220316d82b441409fb9
hassanuxbert <155441517+hassanuxbert@users.noreply.github.com>
2024-08-07 15:46:19 +0500

    UAT FIX: Forgot Password Screen Updated With RHF & Validation Added. (#749)
    * refactor:id confirmation component updated,iqamaId input updated,  React hook form provider and global validation scema applied now
    
    * fix: file name fix, and imports updated

--- app/src/screens/auth/forgot-passcode/identity-confirmation.component.tsx
+++ app/src/screens/auth/forgot-passcode/identity-confirmation.component.tsx
@@ -1,12 +1,12 @@
+import React from 'react'
 import { IPayIcon, IPaySpinner, IPayView } from '@app/components/atoms';
-import { IPayAnimatedTextInput, IPayButton, IPayPageDescriptionText } from '@app/components/molecules';
+import { IPayRHFAnimatedTextInput as IPayAnimatedTextInput, IPayButton, IPayPageDescriptionText } from '@app/components/molecules';
 import { useToastContext } from '@app/components/molecules/ipay-toast/context/ipay-toast-context';
 import constants from '@app/constants/constants';
 import useLocalization from '@app/localization/hooks/localization.hook';
 import { prepareForgetPasscode } from '@app/network/services/core/prepare-forget-passcode/prepare-forget-passcode.service';
 import { useTypedDispatch, useTypedSelector } from '@app/store/store';
 import useTheme from '@app/styles/hooks/theme.hook';
-import { regex } from '@app/styles/typography.styles';
 import icons from '@assets/icons';
 import { useState } from 'react';
 import { scale, verticalScale } from 'react-native-size-matters';
@@ -16,12 +16,13 @@ import { setToken } from '@app/network/client';
 import { encryptData } from '@app/network/utilities/encryption-helper';
 import ForgotPasscodeStyles from './forgot.passcode.styles';
 import { SetPasscodeComponentProps } from './forget-passcode.interface';
+import * as Yup from 'yup';
+import IPayFormProvider from '@app/components/molecules/ipay-form-provider/ipay-form-provider.component';
+import { getValidationSchemas } from '@app/services/validation-service';
 
 const IdentityConfirmationComponent: React.FC<SetPasscodeComponentProps> = ({ onCallback, onPressHelp }) => {
   const dispatch = useTypedDispatch();
   const { colors } = useTheme();
-  const [iqamaId, setIqamaId] = useState<string>('');
-  const [iqamaIdErrorMsg, setIqamaIdErrorMsg] = useState<string>('');
   const [apiError, setAPIError] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isLoadingEncrptData, setIsLoadingEncrptData] = useState<boolean>(false);
@@ -29,6 +30,10 @@ const IdentityConfirmationComponent: React.FC<SetPasscodeComponentProps> = ({ on
   const localizationText = useLocalization();
   const { showToast } = useToastContext();
   const { appData } = useTypedSelector((state) => state.appDataReducer);
+
+  const validationSchema = Yup.object().shape({
+    iqamaId: getValidationSchemas(localizationText).iqamaIdSchema,
+  });
 
   const renderToast = (toastMsg: string) => {
     showToast({
@@ -48,10 +53,11 @@ const IdentityConfirmationComponent: React.FC<SetPasscodeComponentProps> = ({ on
       passwordEncryptionKey: string;
     },
     transactionId: string,
+    iqamaId: string,
   ) => {
     setIsLoading(true);
     const encryptedPoiNumber = encryptData(
-      `${encryptedData.passwordEncryptionPrefix}${iqamaId.toString()}`,
+      `${encryptedData.passwordEncryptionPrefix}${iqamaId}`,
       encryptedData.passwordEncryptionKey,
     );
     try {
@@ -79,7 +85,7 @@ const IdentityConfirmationComponent: React.FC<SetPasscodeComponentProps> = ({ on
     }
   };
 
-  const prepareEncryptionData = async () => {
+  const prepareEncryptionData = async (iqamaId: string) => {
     setIsLoadingEncrptData(true);
     const apiResponse = await prepareLogin();
     if (apiResponse.status.type === 'SUCCESS') {
@@ -90,33 +96,21 @@ const IdentityConfirmationComponent: React.FC<SetPasscodeComponentProps> = ({ on
         }),
       );
       setToken(apiResponse?.headers?.authorization);
-      prepareForgetPass(apiResponse?.response, apiResponse?.authentication?.transactionId);
+      prepareForgetPass(apiResponse?.response, apiResponse?.authentication?.transactionId, iqamaId);
     }
     setIsLoadingEncrptData(false);
   };
 
-  const onPressConfirm = () => {
-    if (iqamaId !== '' && iqamaId.length === constants.IQAMA_ID_NUMBER_LENGTH) {
-      prepareEncryptionData();
-    } else {
-      setIqamaIdErrorMsg(localizationText.COMMON.INCORRECT_IQAMA);
-      renderToast(localizationText.COMMON.INCORRECT_IQAMA);
-    }
-  };
-
-  const onPasscodeChangeText = (text: string) => {
-    const reg = regex.NUMBERS_ONLY; // Matches an empty string or any number of digits
-    if (reg.test(text)) {
-      setIqamaId(text);
-    }
-    setIqamaIdErrorMsg(''); // Reset error message on text change
+  const onSubmit = (data: { iqamaId: string }) => {
+    prepareEncryptionData(data.iqamaId);
   };
 
   const handleOnPressHelp = () => {
     onPressHelp && onPressHelp();
   };
 
-  return (
+  return ( <IPayFormProvider validationSchema={validationSchema} defaultValues={{ iqamaId: '' }}>
+  {({ handleSubmit }) => (
     <IPayView style={styles.identityContainer}>
       {(isLoadingEncrptData || isLoading) && <IPaySpinner />}
       <IPayView style={styles.loginIconView}>
@@ -134,19 +128,17 @@ const IdentityConfirmationComponent: React.FC<SetPasscodeComponentProps> = ({ on
       <IPayView style={styles.inputFieldsContainer}>
         <IPayView style={styles.inputTextView}>
           <IPayAnimatedTextInput
+            name="iqamaId"
             label={localizationText.COMMON.ID_IQAMA}
             editable
-            isError={!!iqamaIdErrorMsg}
-            assistiveText={iqamaIdErrorMsg}
-            value={iqamaId}
-            onChangeText={onPasscodeChangeText}
+            keyboardType="decimal-pad"
             maxLength={constants.IQAMA_ID_NUMBER_LENGTH}
           />
         </IPayView>
       </IPayView>
 
       <IPayButton
-        onPress={onPressConfirm}
+        onPress={handleSubmit(onSubmit)}
         btnType="primary"
         btnText={localizationText.COMMON.NEXT}
         large
@@ -162,6 +154,8 @@ const IdentityConfirmationComponent: React.FC<SetPasscodeComponentProps> = ({ on
         rightIcon={<IPayIcon icon={icons.messageQuestion} size={20} color={colors.primary.primary500} />}
       />
     </IPayView>
+       )}
+   </IPayFormProvider>
   );
 };
 
