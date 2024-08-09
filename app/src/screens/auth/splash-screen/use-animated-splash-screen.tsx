5b754a8d0870c4511f26527b854c38eb919372e7
mariauxbert <maria.c@uxbert.com>
2024-08-09 05:27:44 +0500

    Merge remote-tracking branch 'origin/release/ipay-004-sprint4' into hotfix/setup-biometric

--- app/src/screens/auth/splash-screen/use-animated-splash-screen.tsx
+++ app/src/screens/auth/splash-screen/use-animated-splash-screen.tsx
@@ -1,12 +1,8 @@
 import constants from '@app/constants/constants';
-import { permissionsStatus } from '@app/enums/permissions-status.enum';
-import PermissionTypes from '@app/enums/permissions-types.enum';
-import useLocation from '@app/hooks/location.hook';
 import { fadeIn, parallelAnimations, scale } from '@app/ipay-animations/ipay-animations';
 import { navigate, navigateAndReset } from '@app/navigation/navigation-service.navigation';
 import screenNames from '@app/navigation/screen-names.navigation';
 import { setAppData } from '@app/store/slices/app-data-slice';
-import prepareLogin from '@app/network/services/authentication/prepare-login/prepare-login.service';
 import { useTypedDispatch, useTypedSelector } from '@app/store/store';
 import { useNavigation } from '@react-navigation/native';
 import { useEffect, useRef } from 'react';
@@ -21,22 +17,14 @@ const useSplashScreenAnimations = () => {
   const { isFirstTime, isLinkedDevice } = useTypedSelector((state) => state.appDataReducer.appData);
   const dispatch = useTypedDispatch();
 
-  const { permissionStatus, retryPermission } = useLocation(PermissionTypes.LOCATION, true);
-
-  const handlePermissionAndNavigate = async () => {
-    if (permissionStatus !== permissionsStatus.GRANTED) {
-      await retryPermission();
-    }
-
-    if (permissionStatus === permissionsStatus.GRANTED) {
-      if (isFirstTime) {
-        dispatch(setAppData({ isFirstTime: false }));
-        navigate(screenNames.ONBOARDING);
-      } else if (isLinkedDevice) {
-        navigate(screenNames.LOGIN_VIA_PASSCODE);
-      } else {
-        navigateAndReset(screenNames.MOBILE_IQAMA_VERIFICATION);
-      }
+  const handleNavigation = async () => {
+    if (isFirstTime) {
+      dispatch(setAppData({ isFirstTime: false }));
+      navigate(screenNames.ONBOARDING);
+    } else if (isLinkedDevice) {
+      navigate(screenNames.LOGIN_VIA_PASSCODE);
+    } else {
+      navigateAndReset(screenNames.MOBILE_IQAMA_VERIFICATION);
     }
   };
 
@@ -51,13 +39,13 @@ const useSplashScreenAnimations = () => {
 
       setTimeout(async () => {
         await fadeIn(blurAnim, animationDurations.duration1000).start(async () => {
-          await handlePermissionAndNavigate();
+          await handleNavigation();
         });
       }, animationDurations.duration1000);
     };
 
     runAnimations();
-  }, [dispatch, isLinkedDevice, opacityAnim, scaleAnim, blurAnim, navigation, permissionStatus]);
+  }, [dispatch, isLinkedDevice, opacityAnim, scaleAnim, blurAnim, navigation]);
 
   return { opacityAnim, scaleAnim, blurAnim };
 };
