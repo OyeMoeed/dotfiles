7f40cf0d6b50251bd9e5d3f42e72bf6bd6ab1d4c
mariauxbert <maria.c@uxbert.com>
2024-08-10 19:57:03 +0500

    Merge remote-tracking branch 'origin/release/ipay-004-sprint4' into hotfix/wallet-info-qr-code

--- app/src/components/templates/otp-verification/ipay-otp-verification.hook.ts
+++ app/src/components/templates/otp-verification/ipay-otp-verification.hook.ts
@@ -1,7 +1,11 @@
-import { DURATIONS, INITIAL_TIMER } from '@app/constants/constants';
+import constants from '@app/constants/constants';
+import { useTypedSelector } from '@app/store/store';
+import { minutesToSeconds } from '@app/utilities/date-helper.util';
 import { useEffect, useImperativeHandle, useRef, useState } from 'react';
 
 export const useOtpVerification = (setOtp: (otp: string) => void, setOtpError: (error: boolean) => void) => {
+  const { otpTimeout } = useTypedSelector((state) => state.appDataReducer.appData);
+  const INITIAL_TIMER = minutesToSeconds(otpTimeout) || constants.INITIAL_TIMER;
   const [counter, setCounter] = useState(INITIAL_TIMER);
   const endTimeRef = useRef<number>(Date.now() + INITIAL_TIMER * 1000);
   const timerRef = useRef<any>(null);
