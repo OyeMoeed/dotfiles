0b5fbc35684522c7ba59a50dc97baaa522b08dec
mariauxbert <maria.c@uxbert.com>
2024-08-11 15:23:02 +0500

    Merge remote-tracking branch 'origin/release/ipay-004-sprint4' into hotfix/spinner-overlay

--- app/src/screens/nearest-atm/nearest-atm.screen.tsx
+++ app/src/screens/nearest-atm/nearest-atm.screen.tsx
@@ -16,7 +16,6 @@ import { TabBase } from '@app/utilities/enums.util';
 import { bottomSheetTypes } from '@app/utilities/types-helper.util';
 import React, { useEffect, useRef, useState } from 'react';
 import { Linking, Platform } from 'react-native';
-import { bottomSheetTypes } from '@app/utilities/types-helper.util';
 import NearestAtmListComponent from './nearest-atm-list-component';
 import { AtmDetailsProps } from './nearest-atm-list.interface';
 import nearestAtmStyles from './nearest-atm.style';
