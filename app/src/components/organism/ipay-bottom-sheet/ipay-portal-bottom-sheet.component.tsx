6a8d933666837a7a13cfe3771fa1f3bfb0d87526
mariauxbert <maria.c@uxbert.com>
2024-08-13 17:35:37 +0500

    feat: bottom portal

---
+++ app/src/components/organism/ipay-bottom-sheet/ipay-portal-bottom-sheet.component.tsx
@@ -0,0 +1,152 @@
+import { IPayLinearGradientView } from '@app/components/atoms';
+import { ToastProvider } from '@app/components/molecules/ipay-toast/context/ipay-toast-context';
+import useTheme from '@app/styles/hooks/theme.hook';
+import BottomSheet, { BottomSheetBackdrop, BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet';
+import { forwardRef, useCallback, useImperativeHandle, useMemo, useRef } from 'react';
+import { Platform } from 'react-native';
+import { Portal } from 'react-native-portalize';
+import IPayBottomSheetHandle from './ipay-bottom-sheet-handle.component';
+import { IPayBottomSheetProps } from './ipay-bottom-sheet.interface';
+import bottonSheetStyles from './ipay-bottom-sheet.style';
+import FullWindowOverlay from './ipay-full-window-overlay';
+const IPayPortalBottomSheet = forwardRef<BottomSheetModal, IPayBottomSheetProps>(
+  (
+    {
+      children,
+      customSnapPoint,
+      enableDynamicSizing,
+      simpleHeader,
+      heading,
+      enablePanDownToClose,
+      simpleBar,
+      gradientBar,
+      cancelBnt,
+      doneBtn,
+      backBtn,
+      doneText,
+      onCloseBottomSheet,
+      bold,
+      doneButtonStyle,
+      cancelButtonStyle,
+      onDone,
+      isPanningGesture = false,
+      closeBottomSheetOnDone = true,
+      bottomSheetBgStyles,
+      bgGradientColors,
+      headerContainerStyles,
+      noGradient,
+      animate = true,
+      isVisible = false,
+    },
+    ref,
+  ) => {
+    const { colors } = useTheme();
+    const styles = bottonSheetStyles(colors);
+    const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+    const newSnapPoint = customSnapPoint || ['25%', '50%', '80%'];
+    const snapPoints = useMemo(() => newSnapPoint, [customSnapPoint]);
+    const gradient = bgGradientColors || colors.bottomsheetGradient;
+
+    const handlePresentModalPress = useCallback(() => {
+      bottomSheetModalRef.current?.present();
+    }, []);
+
+    const handleSheetChanges = useCallback(() => {}, []);
+
+    const containerComponent = useCallback((props: any) => <FullWindowOverlay>{props.children}</FullWindowOverlay>, []);
+
+    const onPressClose = () => {
+      setTimeout(() => {
+        bottomSheetModalRef.current?.forceClose();
+      }, 0);
+    };
+
+    const onPressDone = () => {
+      if (onDone) onDone();
+      if (closeBottomSheetOnDone) onPressClose();
+    };
+
+    useImperativeHandle(ref, () => ({
+      present: handlePresentModalPress,
+      close: onPressClose,
+      dismiss: () => bottomSheetModalRef.current?.dismiss(),
+      snapToIndex: (index: number) => bottomSheetModalRef.current?.snapToIndex(index),
+      snapToPosition: (position: string | number) => bottomSheetModalRef.current?.snapToPosition(position),
+      expand: () => bottomSheetModalRef.current?.expand(),
+      collapse: () => bottomSheetModalRef.current?.collapse(),
+      forceClose: () => bottomSheetModalRef.current?.forceClose(), // Add forceClose method
+    }));
+
+    const onAnimate = (fromIndex: number, toIndex: number) => {
+      if (toIndex < 1) {
+        bottomSheetModalRef.current?.forceClose();
+        if (onCloseBottomSheet) onCloseBottomSheet();
+      }
+    };
+
+    // renders
+    const renderBackdrop = useCallback(
+      (props) => (
+        <BottomSheetBackdrop pressBehavior="close" {...props} opacity={1} style={[props.style, styles.overlayStyle]} />
+      ),
+      [],
+    );
+
+    if (!isVisible) {
+      return null;
+    }
+    return (
+      <Portal>
+        <BottomSheet
+          keyboardBehavior="fillParent"
+          backdropComponent={renderBackdrop}
+          name="BottomSheet"
+          enableDismissOnClose
+          onDismiss={animate ? () => bottomSheetModalRef.current?.close() : onCloseBottomSheet}
+          ref={bottomSheetModalRef}
+          index={1}
+          snapPoints={snapPoints}
+          onChange={handleSheetChanges}
+          onAnimate={animate && onAnimate}
+          stackBehavior="push"
+          backgroundStyle={[styles.backgroundStyle, bottomSheetBgStyles]}
+          enableDynamicSizing={enableDynamicSizing}
+          enablePanDownToClose={enablePanDownToClose}
+          enableContentPanningGesture={isPanningGesture}
+          containekyrComponent={Platform.OS === 'ios' ? containerComponent : undefined}
+          handleComponent={() => (
+            <IPayBottomSheetHandle
+              simpleBar={simpleBar}
+              gradientBar={gradientBar}
+              cancelBnt={cancelBnt}
+              doneBtn={doneBtn}
+              heading={heading}
+              simpleHeader={simpleHeader}
+              backBtn={backBtn}
+              doneButtonStyle={doneButtonStyle}
+              cancelButtonStyle={cancelButtonStyle}
+              doneText={doneText}
+              onPressCancel={onPressClose}
+              onPressDone={onPressDone}
+              bold={bold}
+              bgGradientColors={
+                noGradient ? [colors.backgrounds.greyOverlay, colors.backgrounds.greyOverlay] : bgGradientColors
+              }
+              headerContainerStyles={[headerContainerStyles, noGradient && styles.borderRadius]}
+            />
+          )}
+        >
+          <IPayLinearGradientView
+            gradientColors={noGradient ? [colors.backgrounds.greyOverlay, colors.backgrounds.greyOverlay] : gradient}
+          >
+            <ToastProvider>
+              <BottomSheetView style={styles.contentContainer}>{children}</BottomSheetView>
+            </ToastProvider>
+          </IPayLinearGradientView>
+        </BottomSheet>
+      </Portal>
+    );
+  },
+);
+
+export default IPayPortalBottomSheet;
