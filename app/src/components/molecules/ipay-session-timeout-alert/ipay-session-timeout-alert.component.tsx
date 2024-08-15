9a4fd165fd14db8f6925d2174e5a6bf90ba617d0
mariauxbert <maria.c@uxbert.com>
2024-08-15 13:55:59 +0500

    fix:timeout

--- app/src/components/molecules/ipay-session-timeout-alert/ipay-session-timeout-alert.component.tsx
+++ app/src/components/molecules/ipay-session-timeout-alert/ipay-session-timeout-alert.component.tsx
@@ -1,6 +1,7 @@
 import IPayAlert from '@app/components/atoms/ipay-alert/ipay-alert.component';
 import useLocalization from '@app/localization/hooks/localization.hook';
 import { logoutProcess } from '@app/network/utilities/network-session-helper';
+import { hideSessionTimeoutAlert } from '@app/store/slices/alert-slice';
 import { store } from '@app/store/store';
 import { alertVariant } from '@app/utilities/enums.util';
 import { FC } from 'react';
@@ -11,6 +12,7 @@ const IPaySessionTimeoutAlert: FC<IPaySessionTimeoutAlertProps> = ({ visible, te
   const onClose = async () => {
     const { auth } = store.getState();
     if (auth?.isAuthorized) {
+      store.dispatch(hideSessionTimeoutAlert());
       await logoutProcess();
     }
   };
