b990d70bde708b4f50e5065f67335070e7996975
Talib Hussain <talib.c@uxbert.com>
2024-08-18 19:17:09 +0500

    fix: quick url fix

--- app/src/network/services/local-transfer/local-transfer.urls.ts
+++ app/src/network/services/local-transfer/local-transfer.urls.ts
@@ -1,6 +1,7 @@
 const LOCAL_TRANSFERS_URLS = {
-  GET_LOCAL_TRANSFER_BENEFICIARIES_METADATA: () => 'v1/v1/alinma-pay/beneficiaries/local/metadata',
-  GET_LOCAL_TRANSFER_BENEFICIARIES: () => 'v1/v1/alinma-pay/beneficiaries/local',
+  GET_LOCAL_TRANSFER_BENEFICIARIES_METADATA: () =>
+    '/alinmapay/transfer-management/v1/alinma-pay/beneficiaries/local/metadata',
+  GET_LOCAL_TRANSFER_BENEFICIARIES: () => '/alinmapay/transfer-management/v1/alinma-pay/beneficiaries/local',
 };
 
 export default LOCAL_TRANSFERS_URLS;
