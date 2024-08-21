7c3f750f93bac75d36b0f3506565db8b720e1e39
HassanRaza <hassan.c@uxbert.com>
2024-08-21 18:36:58 +0500

    chore: added a selector to check the tier level of the user , reusable across the app

--- app/src/store/slices/user-information-slice.ts
+++ app/src/store/slices/user-information-slice.ts
@@ -1,4 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+import { WALLET_TIERS } from '@app/constants/constants';
 
 /**
  * Interface representing the initial state shape for user information.
@@ -65,6 +67,14 @@ const userInfoSlice = createSlice({
  * Action creators for setting the user info and login data.
  */
 export const { setUserInfo } = userInfoSlice.actions;
+
+/**
+ * Selectors for setting the user info and login data.
+ */
+export const isBasicTierSelector = (state: RootState) => {
+  const { basicTier, walletTier } = state.userInfoReducer.userInfo;
+  return !(walletTier == WALLET_TIERS.BASIC && basicTier);
+};
 
 /**
  * Reducer function for the user info slice.
