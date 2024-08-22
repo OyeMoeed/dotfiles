76019699053ce9f330e0110b615d6c8116780e86
aamossa <aamossa@alinma.com>
2024-08-22 11:51:40 +0300

    hotfix: format contacts mobile & name

--- app/src/constants/app-validations.ts
+++ app/src/constants/app-validations.ts
@@ -2,6 +2,8 @@ const REGEX = {
   nationalID: /^(1|2)([0-9]{9})$/,
   SaudiMobileNumber: /^(05)([0-9]{8})$/,
   LongSaudiMobileNumber: /^(9665)([0-9]{8})$/,
+  longSaudiMobileNumber2: /^(\+9665)([0-9]{8})$/,
+  longSaudiMobileNumber3: /^(009665)([0-9]{8})$/,
   IBAN: /^(SA)([A-Za-z0-9]){22}$/,
   DIGITS_ONLY: /^\d+$/,
 };
