97e2549ac79c3568c65973f6a1c0d26f37f5b008
JavedUxbert <154338382+JavedUxbert@users.noreply.github.com>
2024-08-26 12:46:34 +0500

    fix: resolve typo issue for get billers categories services

--- app/src/network/services/bills-management/bills-management.urls.ts
+++ app/src/network/services/bills-management/bills-management.urls.ts
@@ -1,5 +1,5 @@
 const CARDS_MANAGEMENT_URLS = {
-  get_billers_category: () => 'bills-management/v1/billers/biller-categories',
+  get_billers_categories: () => 'bills-management/v1/billers/biller-categories',
   get_billers_services: (billerID: string) => `bills-management/v1/alinma-payments/billers/${billerID}/services`,
 };
 
