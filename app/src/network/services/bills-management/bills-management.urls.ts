6d120351f4e0038f9df00274c4e59d485a83213f
JavedUxbert <154338382+JavedUxbert@users.noreply.github.com>
2024-08-26 12:57:32 +0500

    fix: type resolve on bills management url file

--- app/src/network/services/bills-management/bills-management.urls.ts
+++ app/src/network/services/bills-management/bills-management.urls.ts
@@ -1,6 +1,6 @@
-const CARDS_MANAGEMENT_URLS = {
+const BILLS_MANAGEMENT_URLS = {
   get_billers_categories: () => 'bills-management/v1/billers/biller-categories',
   get_billers_services: (billerID: string) => `bills-management/v1/alinma-payments/billers/${billerID}/services`,
 };
 
-export default CARDS_MANAGEMENT_URLS;
+export default BILLS_MANAGEMENT_URLS;
