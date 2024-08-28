2e88c419ac9fd3555e36b9a23782884044d9ed93
JavedUxbert <154338382+JavedUxbert@users.noreply.github.com>
2024-08-28 09:47:29 +0500

    feat: setup api for inquire bill, created mock, test, service and interface

---
+++ app/src/network/services/bills-management/inquire-bill/inquire-bill.service.ts
@@ -0,0 +1,30 @@
+import constants from '@app/constants/constants';
+import requestType from '@app/network/request-types.network';
+import apiCall from '@network/services/api-call.service';
+import { ApiResponseStatusType } from '@app/utilities/enums.util';
+import BILLS_MANAGEMENT_URLS from '../bills-management.urls';
+import { InquireBillResponseTypes, InquireBillPayloadTypes } from './inquire-bill.interface';
+import inquireBillMockResponse from './inquire-bill.mock';
+
+const getBillersService = async (payload: InquireBillPayloadTypes): Promise<InquireBillResponseTypes> => {
+  if (constants.MOCK_API_RESPONSE) {
+    return inquireBillMockResponse;
+  }
+  try {
+    const apiResponse: InquireBillResponseTypes = await apiCall({
+      endpoint: BILLS_MANAGEMENT_URLS.inquire_bill(),
+      method: requestType.POST,
+      payload,
+    });
+
+    if (apiResponse?.status?.type === ApiResponseStatusType.SUCCESS) {
+      return apiResponse;
+    }
+
+    return { apiResponseNotOk: true, apiResponse };
+  } catch (error: any) {
+    return { error: error.message || 'Unknown error' };
+  }
+};
+
+export default getBillersService;
