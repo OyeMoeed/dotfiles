1de592abb8deb94057c4f3b91f1decd48447ad44
aamossa <aamossa@alinma.com>
2024-08-28 16:47:55 +0300

    hotfix: disable biometric after (change or reset) passcode or delink

--- app/src/screens/auth/confirm-reset/confirm-reset.screen.tsx
+++ app/src/screens/auth/confirm-reset/confirm-reset.screen.tsx
@@ -13,7 +13,7 @@ import useBiometricService from '@app/network/services/core/biometric/biometric-
 import { ChangePasswordProps } from '@app/network/services/core/change-passcode/change-passcode.interface';
 import changePasscodeReq from '@app/network/services/core/change-passcode/change-passcode.service';
 import { encryptData } from '@app/network/utilities/encryption-helper';
-import { useTypedDispatch, useTypedSelector } from '@app/store/store';
+import { useTypedSelector } from '@app/store/store';
 import useTheme from '@app/styles/hooks/theme.hook';
 import { spinnerVariant } from '@app/utilities/enums.util';
 import { forwardRef, useState } from 'react';
