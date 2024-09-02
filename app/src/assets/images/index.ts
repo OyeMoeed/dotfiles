75df491faadc43634b7105e3f3830b64054b1fe1
mariauxbert <maria.c@uxbert.com>
2024-09-02 19:50:24 +0500

    fix/ logo issue

--- app/src/assets/images/index.ts
+++ app/src/assets/images/index.ts
@@ -80,7 +80,6 @@ const images = {
   license: require('./licence.png'),
   traffic: require('./traffic.png'),
   offersCover: require('./offers-cover.png'),
-  digitalWallet: require('./digital-wallet.png'),
   mada: require('./mada.png'),
   liscence: require('./licence.png'),
   refresh: require('./refresh.png'),
