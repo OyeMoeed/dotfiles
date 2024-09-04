d5f8b5bb12a6b203c6f272d8dbbb20aa2fa57d0d
Muhammad Abdullah <shahab.c@uxbert.com>
2024-09-04 18:45:50 +0500

    feat: added request management sent and received requests services and mocks

---
+++ app/src/network/services/request-management/sent-requests/sent-requests.mock.ts
@@ -0,0 +1,42 @@
+import { GetAllRequestsMockProps } from './sent-requests.interface';
+
+const getAllRequestsMock: GetAllRequestsMockProps = {
+  status: {
+    code: 'I000000',
+    type: 'SUCCESS',
+    desc: 'retail.msg.default.success',
+    sessionReference: 'SSPAYCbe72e8ca732846d68b77117a14a8af83',
+    requestReference: '07118831450799381541',
+  },
+  paginationInfo: {
+    matchedRecords: '20',
+    sentRecords: '20',
+  },
+
+  response: {
+    requests: [
+      {
+        transactionId: 'EPY08099V8QDS',
+        transactionState: 'initiated',
+        transactionTime: '2008-04-08T15:44:00',
+        targetWalletNumber: '10142',
+        targetFullName: 'Mohammed test testFamilyName',
+        targetMobileNumber: '0583431010',
+        targetAmount: '10.00',
+      },
+      {
+        transactionId: 'EPY08099HKN31',
+        transactionState: 'initiated',
+        transactionTime: '2008-04-08T12:19:00',
+        targetWalletNumber: '10142',
+        targetFullName: 'SAHQB ABDULLAH ABDULAHMASAN ALRMOI',
+        targetMobileNumber: '0508987485',
+        targetAmount: '1.00',
+      },
+    ],
+  },
+  successfulResponse: true,
+  ok: true,
+};
+
+export default getAllRequestsMock;
