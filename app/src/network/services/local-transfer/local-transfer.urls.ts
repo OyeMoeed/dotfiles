6583f72ee9e91001242ef8a734e5f386f6d97584
Maria Khan Akhtar <maria.c@uxbert.com>
2024-09-08 00:40:39 +0300

    update url

--- app/src/network/services/local-transfer/local-transfer.urls.ts
+++ app/src/network/services/local-transfer/local-transfer.urls.ts
@@ -1,7 +1,6 @@
 const LOCAL_TRANSFERS_URLS = {
-  get_local_transfer_beneficiaries_metadata: () =>
-    '/alinmapay/transfer-management/v1/alinma-pay/beneficiaries/local/metadata',
-  validateIBAN: () => `/alinmapay/transfer-management/v1/alinma-pay/beneficiaries/iban-validty/`,
+  get_local_transfer_beneficiaries_metadata: () => '/transfer-management/v1/alinma-pay/beneficiaries/local/metadata',
+  validateIBAN: () => '/transfer-management/v1/alinma-pay/beneficiaries/iban-validty/',
   get_local_transfer_beneficiaries: () => '/alinmapay/transfer-management/v1/alinma-pay/beneficiaries/local',
   get_local_beneficiaries_bank_details: () => '/alinmapay-soft/transfer-management/v1/alinma-pay/beneficiaries/',
   add_local_transfer_beneficiary: () => '/alinmapay-soft/transfer-management/v1/alinma-pay/beneficiaries/local',
