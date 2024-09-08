5a160a5aa180328b499fc94bb422f782d961f4e2
Ahmad Taha <aktaha@alinma.com>
2024-09-08 15:11:39 +0300

    Fix: fix imports in svgs index file

--- app/src/assets/svgs/index.tsx
+++ app/src/assets/svgs/index.tsx
@@ -19,18 +19,20 @@ import ExportIcon from './export.svg';
 import GradientCard from './gradient.card.svg';
 import GradientCvv from './gradient.cvv.svg';
 import Home2 from './home-2.svg';
+import IncomingCall from './incoming.svg';
 import AlinmaLogo from './logo.svg';
 import LogoBlueIcon from './logoIconBlue.svg';
 import MasterCard from './masterCard.svg';
 import MenuIcon from './menu.svg';
+import Outgoing from './outgoing.svg';
 import Play from './play.svg';
 import PointRedemptionBackground from './point-redemption-background.svg';
 import PointRedemptionPlus from './point-redemption-plus.svg';
 import ProfileIcon from './profile-circle.svg';
 import ReceiptIcon from './receipt-item1.svg';
 import Refresh2Icon from './refresh-2.svg';
-import Send2Icon from './send-2.svg';
 import RefreshIcon from './refresh.svg';
+import Send2Icon from './send-2.svg';
 import ShoppingCartIcon from './shopping-cart.svg';
 import TrashIcon from './trash.svg';
 
@@ -68,8 +70,8 @@ export {
   Question,
   ReceiptIcon,
   Refresh2Icon,
-  Send2Icon,
   RefreshIcon,
+  Send2Icon,
   ShoppingCartIcon,
   TrashIcon,
   UserTick,
