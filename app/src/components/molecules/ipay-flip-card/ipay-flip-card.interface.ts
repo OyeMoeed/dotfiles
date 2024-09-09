376d596ea718df628e6b91bfef1be665164f86b7
Talib Hussain <talib.c@uxbert.com>
2024-09-09 16:17:45 +0500

    refactor: code improvements

--- app/src/components/molecules/ipay-flip-card/ipay-flip-card.interface.ts
+++ app/src/components/molecules/ipay-flip-card/ipay-flip-card.interface.ts
@@ -8,5 +8,4 @@ export interface FlipCardProps {
   backViewComponent: React.ReactNode;
   returnFilpedIndex?: (index: number) => void;
   isExpired?: boolean;
-  expiredOverlayComponent?: React.ReactNode;
 }
