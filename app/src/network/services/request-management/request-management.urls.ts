75bae27eb372c73ddf5298390957a6cfb8007cee
JavedUxbert <154338382+JavedUxbert@users.noreply.github.com>
2024-09-09 15:30:45 +0500

    feat: create service, interface, mock and test cases for reject and cancel request api

--- app/src/network/services/request-management/request-management.urls.ts
+++ app/src/network/services/request-management/request-management.urls.ts
@@ -1,8 +1,12 @@
+import UpdateRequestTypes from './update-request.types';
+
 const REQUEST_MANAGEMENT_URLS = {
   getAllRequests: (walletNumber?: string) => `transfer-management/v1/${walletNumber}/money-requests`,
   recevied_request_prepare: (walletNumber: string, transactionId: string) =>
     `transfer-management/v1/${walletNumber}/money-requests/${{ transactionId }}/accept/prepare`,
   recevied_request_confirm: (walletNumber: string, transactionId: string) =>
     `transfer-management/v1/${walletNumber}/money-requests/${{ transactionId }}/accept/prepare`,
+  cancelRejectRequest: (walletNumber: string, transactionId: string, type: UpdateRequestTypes) =>
+    `transfer-management/v1/${walletNumber}/money-requests/${transactionId}/manage?function=${type}`,
 };
 export default REQUEST_MANAGEMENT_URLS;
