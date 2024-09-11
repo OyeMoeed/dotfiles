8a6879173dc50c5dd8c6905832fe5fa22a143fbc
shakeelmazhar10 <shakeel.c@uxbert.com>
2024-09-11 18:27:59 +0500

    fix: URL endpoints for marketplace

--- app/src/network/services/market/market.urls.ts
+++ app/src/network/services/market/market.urls.ts
@@ -1,14 +1,14 @@
 const MARKET_URLS = {
   CHECK_AP_VOUCHER_ISACTIVE: 'market/v1/ap-vouchers/is-active',
-  GET_AP_VOUCHER_IMAGES: 'alinmapay/market/v1/ap-vouchers/images',
-  GET_AP_VOUCHER_CATEGORIES: '/market/v1/ap-vouchers/categories',
-  PREPARE_PURCHASE: 'alinmapay/market/v1/ap-vouchers/purchase/prepare',
-  CONFIRM_PURCHASE: 'alinmapay/market/v1/ap-vouchers/purchase/confirm',
-  GET_SHOP_ORDER_HISTORY: (walletNumber: string) => `alinmapay/market/v1/${walletNumber}/ap-vouchers/purchase`,
+  GET_AP_VOUCHER_IMAGES: 'market/v1/ap-vouchers/images',
+  GET_AP_VOUCHER_CATEGORIES: 'market/v1/ap-vouchers/categories',
+  PREPARE_PURCHASE: 'market/v1/ap-vouchers/purchase/prepare',
+  CONFIRM_PURCHASE: 'market/v1/ap-vouchers/purchase/confirm',
+  GET_SHOP_ORDER_HISTORY: (walletNumber: string) => `market/v1/${walletNumber}/ap-vouchers/purchase`,
   GET_AP_VOUCHER_MERCHANTS_CATEGORY: (categoryId: number | string) =>
-    `/alinmapay/market/v1/ap-vouchers/merchants?category=${categoryId}`,
+    `market/v1/ap-vouchers/merchants?category=${categoryId}`,
   GET_PRODUCT_DETAILS_BY_PRODUCT_ID: (merchantId: string, productId: string) =>
-    `alinmapay/market/v1/ap-vouchers/products?merchant=${merchantId}&product=${productId}`,
+    `market/v1/ap-vouchers/products?merchant=${merchantId}&product=${productId}`,
 };
 
 export default MARKET_URLS;
