fc80a8e74e031c540d9015970e9ac29776287203
HassanRaza <hassan.c@uxbert.com>
2024-09-12 13:28:26 +0500

    chore: updated T&C sheet with global sheet on issue new card screen

--- app/src/screens/issue-new-card-confirm-details/issue-new-card-confirm-details.screen.tsx
+++ app/src/screens/issue-new-card-confirm-details/issue-new-card-confirm-details.screen.tsx
@@ -1,6 +1,4 @@
-import React, { useRef, useState } from 'react';
-import { IPayButton, IPayHeader, IPayList } from '@app/components/molecules';
-import { IPayOtpVerification, IPaySafeAreaView } from '@components/templates';
+import icons from '@app/assets/icons';
 import {
   IPayCheckbox,
   IPayFootnoteText,
@@ -10,26 +8,26 @@ import {
   IPaySubHeadlineText,
   IPayView,
 } from '@app/components/atoms';
+import { IPayButton, IPayHeader, IPayList } from '@app/components/molecules';
 import IPayAccountBalance from '@app/components/molecules/ipay-account-balance/ipay-account-balance.component';
-import { IPayBottomSheet, IPayTermsAndConditions } from '@app/components/organism';
-import useLocalization from '@app/localization/hooks/localization.hook';
-import useTheme from '@app/styles/hooks/theme.hook';
-import { buttonVariants } from '@app/utilities/enums.util';
-import { useRoute, RouteProp } from '@react-navigation/native';
-import icons from '@app/assets/icons';
-import bottomSheetModal from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetModal';
-import IPayAddressInfoSheet from '@app/components/organism/ipay-address-info-sheet/ipay-address-info-sheet.component';
 import { useToastContext } from '@app/components/molecules/ipay-toast/context/ipay-toast-context';
+import { IPayBottomSheet } from '@app/components/organism';
+import IPayAddressInfoSheet from '@app/components/organism/ipay-address-info-sheet/ipay-address-info-sheet.component';
+import useLocalization from '@app/localization/hooks/localization.hook';
 import { navigate } from '@app/navigation/navigation-service.navigation';
 import ScreenNames from '@app/navigation/screen-names.navigation';
+import { setTermsConditionsVisibility } from '@app/store/slices/nafath-verification';
+import useTheme from '@app/styles/hooks/theme.hook';
+import { buttonVariants } from '@app/utilities/enums.util';
+import { IPayOtpVerification, IPaySafeAreaView } from '@components/templates';
+import bottomSheetModal from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetModal';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import React, { useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import HelpCenterComponent from '../auth/forgot-passcode/help-center.component';
-import {
-  RouteParams,
-  TermsAndConditionsRefTypes,
-  AddressInfoRefTypes,
-} from './issue-new-card-confirm-details.interface';
-import issueNewCardConfirmDetailsStyles from './issue-new-card-confirm-details.style';
 import IPayCreateCardPin from '../create-card-pin/create-card-pin.screen';
+import { AddressInfoRefTypes, RouteParams } from './issue-new-card-confirm-details.interface';
+import issueNewCardConfirmDetailsStyles from './issue-new-card-confirm-details.style';
 
 const DUMMY_DATA = {
   address: 'Al Olaya, Riyadh, SA',
@@ -58,7 +56,6 @@ const IssueNewCardConfirmDetailsScreen: React.FC = () => {
   const veriyOTPSheetRef = useRef<bottomSheetModal>(null);
   const otpVerificationRef = useRef<bottomSheetModal>(null);
   const helpCenterRef = useRef<bottomSheetModal>(null);
-  const termsAndConditionSheetRef = useRef<TermsAndConditionsRefTypes>(null);
   const openBottomSheet = useRef<bottomSheetModal>(null);
   const styles = issueNewCardConfirmDetailsStyles(colors);
   const addressInfoSheetRef = useRef<AddressInfoRefTypes>(null);
@@ -95,8 +92,13 @@ const IssueNewCardConfirmDetailsScreen: React.FC = () => {
 
   const toggleTermsAndConditions = () => setCheckTermsAndConditions((prev) => !prev);
 
+  const dispatch = useDispatch();
   const onPressTermsAndConditions = () => {
-    termsAndConditionSheetRef.current?.showTermsAndConditions();
+    dispatch(setTermsConditionsVisibility(true));
+  };
+
+  const onResendCodePress = () => {
+    // Add Code Later during API INTEGRATION
   };
 
   const renderToast = () => {
@@ -234,7 +236,7 @@ const IssueNewCardConfirmDetailsScreen: React.FC = () => {
       >
         <IPayCreateCardPin onSuccess={onSuccessPin} />
       </IPayBottomSheet>
-      <IPayTermsAndConditions ref={termsAndConditionSheetRef} />
+
       <IPayBottomSheet
         testID="ipay-bottom-physical-card"
         heading={localizationText.REPLACE_CARD.REPLACE_PHYSICAL_CARD}
@@ -255,6 +257,7 @@ const IssueNewCardConfirmDetailsScreen: React.FC = () => {
           handleOnPressHelp={handleOnPressHelp}
           otp={otp}
           otpError={otpError}
+          onResendCodePress={onResendCodePress} // Add logic Later in Api Integration
         />
       </IPayBottomSheet>
       <IPayBottomSheet
