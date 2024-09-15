fd923782bb0a421f3e17dc91087a0ce4677161ec
Moeed Sarwar <moeed.c@uxbert.com>
2024-09-15 19:08:56 +0500

    fix: fixed color condition

--- app/src/screens/help-center/helpcenter.screen.tsx
+++ app/src/screens/help-center/helpcenter.screen.tsx
@@ -217,7 +217,7 @@ const HelpCenter: React.FC = () => {
           <IPayIcon
             icon={isOpen(index, section.id) ? icons.arrowUp : icons.ARROW_DOWN}
             size={18}
-            color={colors.primary.primary500}
+            color={isOpen(index, section.id) ? colors.primary.primary500 : colors.primary.primary900}
             style={isOpen(index, section.id) ? styles.faqItemIconExpanded : styles.faqItemIcon}
           />
         </IPayView>
