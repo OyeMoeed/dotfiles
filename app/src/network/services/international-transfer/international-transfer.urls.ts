7325001c1f83020a2d3c3de35a68759b7dd61a20
shakeelmazhar10 <shakeel.c@uxbert.com>
2024-09-15 15:07:02 +0500

    fix:  transfer management urls fix

--- app/src/network/services/international-transfer/international-transfer.urls.ts
+++ app/src/network/services/international-transfer/international-transfer.urls.ts
@@ -1,13 +1,13 @@
 const INTERNATIONAL_TRANSFERS_URLS = {
-  get_western_union_beneficiaries: () => ' /v1/alinma-pay/beneficiaries/wu',
+  get_western_union_beneficiaries: () => ' transfer-management/v1/alinma-pay/beneficiaries/wu',
   get_alinma_express_beneficiaries: () => 'transfer-management/v1/alinma-pay/beneficiaries/alinma-express',
-  get_western_union_beneficiaries_metadata: () => '/alinmapay/transfer-management/v1/beneficiaries/wu/metadata',
-  get_western_union_beneficiaries_countries: () => '/alinmapay/transfer-management/v1/beneficiaries/wu/countries',
-  western_union_beneficiaries: () => '/alinmapay/transfer-management/v1/alinma-pay/beneficiaries',
-  western_union_transfer: () => '/alinmapay/transfer-management/v1/alinma-pay/beneficiaries',
-  delete_beneficiary: (beneficiaryCode: string) => `/v1/alinma-pay/beneficiaries${beneficiaryCode}`,
-  activate_beneficiary: () => '/v1/alinma-pay/beneficiaries/activation/mark',
-  edit_beneficiary: (beneficiaryCode: string) => `/v1/alinma-pay/beneficiaries${beneficiaryCode}`,
+  get_western_union_beneficiaries_metadata: () => 'transfer-management/v1/beneficiaries/wu/metadata',
+  get_western_union_beneficiaries_countries: () => 'transfer-management/v1/beneficiaries/wu/countries',
+  western_union_beneficiaries: () => 'transfer-management/v1/alinma-pay/beneficiaries',
+  western_union_transfer: () => 'transfer-management/v1/alinma-pay/beneficiaries',
+  delete_beneficiary: (beneficiaryCode: string) => `transfer-management/v1/alinma-pay/beneficiaries${beneficiaryCode}`,
+  activate_beneficiary: () => 'transfer-management/v1/alinma-pay/beneficiaries/activation/mark',
+  edit_beneficiary: (beneficiaryCode: string) => `transfer-management/v1/alinma-pay/beneficiaries${beneficiaryCode}`,
 };
 
 export default INTERNATIONAL_TRANSFERS_URLS;
