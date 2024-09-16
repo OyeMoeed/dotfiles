5b197acd73128b5929f9cd7184cb4f9a2189bc13
Qais Mahmoud Trad <qmtrad@admins-MacBook-Pro.local>
2024-09-16 14:45:01 +0300

    feat: remove unused packages and setup react query

--- app/App.tsx
+++ app/App.tsx
@@ -4,17 +4,21 @@
  *
  * @format
  */
-import { ToastProvider } from '@app/components/molecules/ipay-toast/context/ipay-toast-context';
-import IPayBottomSheetProvider from '@app/components/organism/ipay-bottomsheet-provider/ipay-bottomsheet-provider.component';
-import MainNavigation from '@app/navigation/app-navigator.navigation';
-import { isIosOS } from '@app/utilities/constants';
-import { persistor, store } from '@store/store';
 import { JSX } from 'react';
+import { QueryClientProvider } from 'react-query';
 import { KeyboardAvoidingView } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Host } from 'react-native-portalize';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+
+import { ToastProvider } from '@app/components/molecules/ipay-toast/context/ipay-toast-context';
+import IPayBottomSheetProvider from '@app/components/organism/ipay-bottomsheet-provider/ipay-bottomsheet-provider.component';
+import MainNavigation from '@app/navigation/app-navigator.navigation';
+import { isIosOS } from '@app/utilities/constants';
+import { persistor, store } from '@store/store';
+import { queryClient } from '@app/network';
+
 import appStyles from './app.styles';
 
 const App = (): JSX.Element => {
@@ -22,17 +26,19 @@ const App = (): JSX.Element => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <KeyboardAvoidingView behavior={isIosOS ? 'padding' : 'height'} style={style.kavStyle}>
-          <GestureHandlerRootView style={style.rootView}>
-            <Host>
-              <ToastProvider>
-                <IPayBottomSheetProvider>
-                  <MainNavigation />
-                </IPayBottomSheetProvider>
-              </ToastProvider>
-            </Host>
-          </GestureHandlerRootView>
-        </KeyboardAvoidingView>
+        <QueryClientProvider client={queryClient}>
+          <KeyboardAvoidingView behavior={isIosOS ? 'padding' : 'height'} style={style.kavStyle}>
+            <GestureHandlerRootView style={style.rootView}>
+              <Host>
+                <ToastProvider>
+                  <IPayBottomSheetProvider>
+                    <MainNavigation />
+                  </IPayBottomSheetProvider>
+                </ToastProvider>
+              </Host>
+            </GestureHandlerRootView>
+          </KeyboardAvoidingView>
+        </QueryClientProvider>
       </PersistGate>
     </Provider>
   );
