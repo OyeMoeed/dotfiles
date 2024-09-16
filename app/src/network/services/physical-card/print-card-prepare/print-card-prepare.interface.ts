cefb5d5c15bdcc1fb45afd63e3c7868168b8c319
JavedUxbert <154338382+JavedUxbert@users.noreply.github.com>
2024-09-16 18:33:01 +0500

    feat: created service, mock, interface and test cases for prepare api

---
+++ app/src/network/services/physical-card/print-card-prepare/print-card-prepare.interface.ts
@@ -0,0 +1,16 @@
+import { MockAPIStatusProps, DeviceInfoProps } from '@network/services/services.interface';
+
+export interface PrintCardPreparePayloadTypes {
+  // deviceInfo: DeviceInfoProps; //as per swagger
+  deviceInfo: DeviceInfoProps; // as per old code
+  cardIndex: string; // as per old code
+}
+
+export interface PrintCardPrepareResponseTypes {
+  status: MockAPIStatusProps;
+  response: {
+    otpRef: string;
+    cardIndex: string; // as per old code
+  };
+  successfulResponse: boolean;
+}
