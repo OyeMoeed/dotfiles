8cf5adc8517d6dba3fb4dc9a033c275b1eb6bbbf
Qais Trad <qmtrad@alinma.com>
2024-10-01 08:58:24 +0300

    feat: musaned salary pay

--- app/src/screens/musaned/musaned-pay-salary/musaned-pay-salary.screen.tsx
+++ app/src/screens/musaned/musaned-pay-salary/musaned-pay-salary.screen.tsx
@@ -1,13 +1,339 @@
-import { IPaySafeAreaView } from '@app/components/templates';
-import { IPayHeader } from '@app/components/molecules';
-
+import icons from '@app/assets/icons';
+import { IPayIcon, IPayScrollView, IPayView } from '@app/components/atoms';
+import { IPayButton, IPayHeader, IPayListView } from '@app/components/molecules';
+import IPayAccountBalance from '@app/components/molecules/ipay-account-balance/ipay-account-balance.component';
+import { ListProps } from '@app/components/molecules/ipay-list-view/ipay-list-view.interface';
+import { useToastContext } from '@app/components/molecules/ipay-toast/context/ipay-toast-context';
+import { IPayBottomSheet, IPayTransferInformation } from '@app/components/organism';
+import { IPaySafeAreaView, IPayTopUpSelection } from '@app/components/templates';
+import { useKeyboardStatus } from '@app/hooks';
+import { navigate } from '@app/navigation/navigation-service.navigation';
+import ScreenNames from '@app/navigation/screen-names.navigation';
+import { useTypedSelector } from '@app/store/store';
+import React, { createRef, useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import getSarieTransferFees from '@app/network/services/cards-management/get-sarie-transfer-fees/get-sarie-transfer-fees.service';
+import { IGetCoreLovPayload } from '@app/network/services/core/lov/get-lov.interface';
+import { getCoreLov } from '@app/network/services/core/lov/get-lov.service';
+import { LocalTransferPreparePayloadTypes } from '@app/network/services/local-transfer/local-transfer-prepare/local-transfer-prepare.interface';
+import localTransferPrepare from '@app/network/services/local-transfer/local-transfer-prepare/local-transfer-prepare.service';
+import { DeviceInfoProps } from '@app/network/services/services.interface';
+import { getDeviceInfo } from '@app/network/utilities';
+import colors from '@app/styles/colors.const';
+import { regex } from '@app/styles/typography.styles';
+import { ApiResponseStatusType, APIResponseType, buttonVariants } from '@app/utilities/enums.util';
+import { removeCommas } from '@app/utilities/number-helper.util';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import IPayPortalBottomSheet from '@app/components/organism/ipay-bottom-sheet/ipay-portal-bottom-sheet.component';
+import { SNAP_POINT } from '@app/constants/constants';
+import getAktharPoints from '@app/network/services/cards-management/mazaya-topup/get-points/get-points.service';
 import musanedPaySalary from './musaned-pay-salary.style';
+import { MusanedPaySalaryScreenProps } from './musaned-pay-salary.interface';
 
-const MusanedPaySalaryScreen = () => {
+const MusanedPaySalaryScreen: React.FC<MusanedPaySalaryScreenProps> = () => {
   const styles = musanedPaySalary();
+  const { t } = useTranslation();
+  const appData = useTypedSelector((state) => state.appDataReducer.appData);
+  const walletInfo = useTypedSelector((state) => state.walletInfoReducer.walletInfo);
+  const { walletNumber } = walletInfo;
+
+  const [chipValue, setChipValue] = useState<string>('');
+  const [transferAmount, setTransferAmount] = useState<string>('');
+  const [selectedReason, setSelectedReason] = useState({});
+  const [notes, setNotes] = useState<string>('');
+  const salaryTypeBottomSheetRef = useRef(null);
+  const [apiError, setAPIError] = useState<string>('');
+  const [transferReason, setTransferReasonData] = useState<ListProps[]>([]);
+
+  const { showToast } = useToastContext();
+
+  type RouteProps = RouteProp<any>;
+  const route = useRoute<RouteProps>();
+  const {
+    beneficiaryBankDetail,
+    nickname: beneficiaryNickName,
+    beneficiaryCode,
+    fullName,
+    beneficiaryAccountNumber,
+  } = route?.params?.beneficiaryDetails || {};
+  const { bankCode, bankName } = beneficiaryBankDetail || {};
+
+  const { limitsDetails, availableBalance, currentBalance } = walletInfo;
+  const { monthlyRemainingOutgoingAmount, dailyRemainingOutgoingAmount, monthlyOutgoingLimit } = limitsDetails;
+
+  const { isKeyboardOpen } = useKeyboardStatus();
+  const bankDetails = {
+    icon: bankCode ?? '',
+    bankName,
+    title: fullName ?? '',
+    accountNumber: beneficiaryAccountNumber ?? '',
+  };
+
+  const transferNetwork = 'IPS'; // TODO need to replace.
+
+  useEffect(() => {
+    const monthlyRemaining = parseFloat(monthlyRemainingOutgoingAmount);
+    const monthlyTotalRemainingLimit = parseFloat(monthlyOutgoingLimit);
+    const updatedTopUpAmount = parseFloat(transferAmount.replace(/,/g, ''));
+
+    if (monthlyRemaining === 0 || updatedTopUpAmount > monthlyRemaining) {
+      setChipValue(t('TOP_UP.AMOUNT_EXCEEDS_CURRENT'));
+    } else if (updatedTopUpAmount > monthlyTotalRemainingLimit) {
+      setChipValue(t('TOP_UP.MONTHLY_SPENDING_LIMIT_REACHED'));
+    } else {
+      setChipValue('');
+    }
+  }, [transferAmount, monthlyRemainingOutgoingAmount, dailyRemainingOutgoingAmount]);
+
+  const setAmount = (text: string | number) => {
+    const newAmount = removeCommas(text.toString());
+    const reg = regex.AMOUNT;
+    if (reg.test(newAmount.toString()) || newAmount === '') {
+      setTransferAmount?.(newAmount.toString());
+    }
+  };
+
+  const isTransferButtonDisabled = () => {
+    const hasValidAmount = parseFloat(transferAmount) > 0 || parseFloat(transferAmount);
+    const hasValidReason = selectedReason?.text?.trim() !== '';
+    return !hasValidAmount || !hasValidReason;
+  };
+  const onCloseSheet = () => {
+    salaryTypeBottomSheetRef?.current?.close();
+  };
+
+  const onPressListItem = (item: ReasonListItem) => {
+    setSelectedReason(item);
+    onCloseSheet();
+  };
+
+  const onPressSelectReason = () => {
+    salaryTypeBottomSheetRef?.current?.present();
+  };
+
+  const renderToast = (toastMsg: string) => {
+    showToast({
+      title: toastMsg,
+      subTitle: apiError,
+      borderColor: colors.error.error25,
+      isShowRightIcon: false,
+      leftIcon: <IPayIcon icon={icons.warning} size={24} color={colors.natural.natural0} />,
+    });
+  };
+
+  const getTransferFee = async () => {
+    if (walletNumber) {
+      try {
+        const apiResponse = await getSarieTransferFees(walletNumber, bankCode, transferAmount);
+        if (apiResponse?.status?.type === APIResponseType.SUCCESS) {
+          return apiResponse?.response;
+        }
+        if (apiResponse?.apiResponseNotOk) {
+          setAPIError(t('ERROR.API_ERROR_RESPONSE'));
+          return null;
+        }
+        setAPIError(apiResponse?.error);
+        return null;
+      } catch (error) {
+        setAPIError(error?.message || t('ERROR.SOMETHING_WENT_WRONG'));
+        renderToast(error?.message || t('ERROR.SOMETHING_WENT_WRONG'));
+        return null;
+      }
+    } else {
+      return null;
+    }
+  };
+
+  const getTotal = (feesAmount: string, vatAmount: string, amount: string) => {
+    const total = Number(vatAmount) + Number(feesAmount) + Number(amount);
+    return total;
+  };
+
+  const onLocalTransferPrepare = async () => {
+    if (transferAmount && selectedReason?.id && walletNumber) {
+      const transferFees = await getTransferFee();
+      if (transferFees) {
+        try {
+          const deviceInfo = await getDeviceInfo();
+          const payload: LocalTransferPreparePayloadTypes = {
+            beneficiaryCode,
+            transferPurpose: selectedReason?.id,
+            feesAmount: transferFees.feeAmount,
+            vatAmount: transferFees.vatAmount,
+            bankFeesAmount: transferFees.bankFeeAmount,
+            bankVatAmount: transferFees.bankVatAmount,
+            amount: transferAmount,
+            note: notes,
+            bankCode,
+            transferNetwork,
+            deviceInfo: {
+              platform: deviceInfo?.platform,
+              platformVersion: deviceInfo?.platformVersion,
+              deviceName: deviceInfo?.deviceName,
+              deviceId: deviceInfo?.deviceId,
+            },
+          };
+          const apiResponse = await localTransferPrepare(walletNumber, payload);
+          if (apiResponse?.status?.type === APIResponseType.SUCCESS) {
+            navigate(ScreenNames.TRANSFER_CONFIRMATION, {
+              amount: transferAmount,
+              beneficiaryNickName,
+              transferPurpose: selectedReason?.text,
+              instantTransferType: t('TRANSFER_SUMMARY.SARIE'),
+              note: notes,
+              otpRef: apiResponse.response.otpRef,
+              feesAmount: transferFees.feeAmount,
+              vatAmount: transferFees.vatAmount,
+              totalAmount: getTotal(transferFees.feeAmount, transferFees.vatAmount, transferAmount),
+              authentication: apiResponse?.authentication,
+              bankDetails,
+            });
+          } else if (apiResponse?.apiResponseNotOk) {
+            setAPIError(t('ERROR.API_ERROR_RESPONSE'));
+          } else {
+            setAPIError(apiResponse?.error);
+          }
+        } catch (error) {
+          setAPIError(error?.message || t('ERROR.SOMETHING_WENT_WRONG'));
+          renderToast(error?.message || t('ERROR.SOMETHING_WENT_WRONG'));
+        }
+      }
+    }
+  };
+
+  const getTransferReasons = async () => {
+    const payload: IGetCoreLovPayload = {
+      lovType: '184',
+      lovCode2: 'W',
+      deviceInfo: (await getDeviceInfo()) as DeviceInfoProps,
+    };
+    const apiResponse = await getCoreLov(payload);
+    if (apiResponse?.status.type === ApiResponseStatusType.SUCCESS) {
+      if (apiResponse?.response?.lovInfo)
+        setTransferReasonData(
+          apiResponse?.response?.lovInfo.map((item) => ({
+            id: item.recTypeCode,
+            text: item.recDescription,
+          })),
+        );
+    }
+  };
+
+  useEffect(() => {
+    getTransferReasons();
+  }, []);
+
+  const [topUpOptionsVisible, setTopUpOptionsVisible] = useState<boolean>(false);
+
+  const topUpSelectionBottomSheet = () => {
+    setTopUpOptionsVisible(true);
+  };
+
+  const closeBottomSheetTopUp = () => {
+    setTopUpOptionsVisible(false);
+  };
+
+  const topUpSelectionRef = createRef<any>();
+
+  const navigateTOAktharPoints = async () => {
+    const aktharPointsResponse = await getAktharPoints(walletNumber);
+    if (
+      aktharPointsResponse?.status?.type === 'SUCCESS' &&
+      aktharPointsResponse?.response?.mazayaStatus !== 'USER_DOES_NOT_HAVE_MAZAYA_ACCOUNT'
+    ) {
+      navigate(ScreenNames.POINTS_REDEMPTIONS, { aktharPointsInfo: aktharPointsResponse?.response, isEligible: true });
+    } else {
+      navigate(ScreenNames.POINTS_REDEMPTIONS, { isEligible: false });
+    }
+  };
+
+  const topupItemSelected = (routeName: string, params: {}) => {
+    closeBottomSheetTopUp();
+    if (routeName === ScreenNames.POINTS_REDEMPTIONS) {
+      navigateTOAktharPoints();
+    } else {
+      navigate(routeName, params);
+    }
+  };
+
   return (
-    <IPaySafeAreaView style={styles.container}>
-      <IPayHeader testID="musaned-user-details-header" backBtn title="MUSANED.HEADER" applyFlex />
+    <IPaySafeAreaView>
+      <IPayHeader backBtn applyFlex title="TRANSFER.TRANSFER_INFRORMATION" />
+      <IPayScrollView>
+        <IPayView style={styles.container}>
+          <IPayAccountBalance
+            balance={availableBalance}
+            availableBalance={currentBalance}
+            hideBalance={appData?.hideBalance}
+            showRemainingAmount
+            onPressTopup={topUpSelectionBottomSheet}
+          />
+
+          <IPayView style={styles.bankDetailsView}>
+            <IPayTransferInformation
+              style={styles.transferContainer}
+              amount={transferAmount}
+              currencyStyle={[styles.currency, transferAmount && styles.inputActiveStyle]}
+              setAmount={setAmount}
+              selectedItem={selectedReason?.text}
+              setNotes={setNotes}
+              notes={notes}
+              chipValue={chipValue}
+              transferInfo
+              transferInfoData={bankDetails}
+              openReason={onPressSelectReason}
+              inputFieldStyle={styles.inputFieldStyle}
+            />
+          </IPayView>
+        </IPayView>
+      </IPayScrollView>
+      {!isKeyboardOpen ? (
+        <IPayView style={styles.buttonContainer}>
+          <IPayButton
+            onPress={onLocalTransferPrepare}
+            btnType={buttonVariants.PRIMARY}
+            large
+            disabled={isTransferButtonDisabled() || Boolean(chipValue)}
+            btnIconsDisabled
+            btnText="COMMON.NEXT"
+            btnStyle={styles.nextBtn}
+          />
+        </IPayView>
+      ) : (
+        <IPayView />
+      )}
+      <IPayBottomSheet
+        heading="MUSANED.SALARY_TYPE"
+        onCloseBottomSheet={onCloseSheet}
+        customSnapPoint={['20%', '65%']}
+        ref={salaryTypeBottomSheetRef}
+        simpleHeader
+        simpleBar
+        cancelBnt
+        bold
+      >
+        <IPayListView list={transferReason} onPressListItem={onPressListItem} selectedListItem={selectedReason?.text} />
+      </IPayBottomSheet>
+
+      <IPayPortalBottomSheet
+        noGradient
+        heading="TOP_UP.ADD_MONEY_USING"
+        onCloseBottomSheet={closeBottomSheetTopUp}
+        customSnapPoint={SNAP_POINT.XS_SMALL}
+        ref={topUpSelectionRef}
+        enablePanDownToClose
+        simpleHeader
+        simpleBar
+        bold
+        cancelBnt
+        isVisible={topUpOptionsVisible}
+      >
+        <IPayTopUpSelection
+          testID="topUp-selection"
+          closeBottomSheet={closeBottomSheetTopUp}
+          topupItemSelected={topupItemSelected}
+        />
+      </IPayPortalBottomSheet>
     </IPaySafeAreaView>
   );
 };
--- app/src/screens/musaned/musaned-pay-salary/musaned-pay-salary.style.ts
+++ app/src/screens/musaned/musaned-pay-salary/musaned-pay-salary.style.ts
@@ -1,11 +1,54 @@
+import { Platform } from 'react-native';
 import { moderateScale } from 'react-native-size-matters';
 
 import createStyleSheet from '@app/styles/scaled-sheet.styles';
+import { FONT_SIZE_17 } from '@app/styles/typography.styles';
 
 const musanedPaySalary = () =>
   createStyleSheet({
     container: {
-      marginBottom: moderateScale(22),
+      flex: 1,
+      marginHorizontal: moderateScale(24, 0.3),
+      marginTop: moderateScale(24, 0.3),
+    },
+    bankDetailsView: {
+      flex: 1,
+    },
+    nextBtn: {
+      marginBottom: moderateScale(46),
+      borderRadius: moderateScale(16),
+      marginHorizontal: moderateScale(24, 0.3),
+    },
+    buttonContainer: {
+      paddingTop: moderateScale(32),
+    },
+    transferContainer: {
+      marginTop: moderateScale(4),
+    },
+    topUpBtnStyle: {
+      paddingHorizontal: moderateScale(14),
+      paddingVertical: moderateScale(7),
+      height: 'auto',
+      borderRadius: moderateScale(12),
+    },
+    inputFieldStyle: {
+      borderRadius: moderateScale(16),
+    },
+    currency: {
+      fontSize: FONT_SIZE_17,
+    },
+    input: {
+      paddingBottom: moderateScale(8),
+    },
+    inputActiveStyle: {
+      ...Platform.select({
+        android: {
+          marginBottom: moderateScale(1),
+        },
+        ios: {
+          marginBottom: 0,
+        },
+      }),
     },
   });
 
