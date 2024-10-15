a0eb37626d242e475620267ee4f004fe376fec52
OyeMoeed <oyemoeed@gmail.com>
2024-10-16 01:46:34 +0500

    removed zen mode

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -53,18 +53,6 @@ return {
       stages = "slide",
     },
   },
-  --zenMode
-  {
-    "folke/zen-mode.nvim",
-    cmd = "ZenMode",
-    opts = {
-      plugins = {
-        gitsigns = true,
-        tmux = true,
-      },
-    },
-    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-  },
   -- buffer line
   {
     "akinsho/bufferline.nvim",
