679673bde30da13a65c0a298cccc94bf94df3e73
OyeMoeed <oyemoeed@gmail.com>
2025-01-29 19:39:12 +0500

    added a file for lzaygit

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -33,6 +33,26 @@ require("lazy").setup({
       end,
     },
 
+    -- lazy git
+    {
+      "kdheepak/lazygit.nvim",
+      cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+      },
+      -- optional for floating window border decoration
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+      -- setting the keybinding for LazyGit with 'keys' is recommended in
+      -- order to load the plugin when the command is run for the first time
+      keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+      },
+    },
     {
       "folke/snacks.nvim",
       priority = 1000,
