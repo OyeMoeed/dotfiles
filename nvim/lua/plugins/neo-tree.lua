baf5a4e39d77390db5d7f188838b68216c805460
OyeMoeed <oyemoeed@gmail.com>
2025-02-21 15:16:14 +0500

    added spotify

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,21 +1,33 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
-  requires = {
+  dependencies = {
     "nvim-lua/plenary.nvim",
-    "echasnovski/mini.icons",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
   config = function()
     require("neo-tree").setup({
       window = {
         position = "right",
-        width = 30, -- Set the width of the Neo-Tree window
+        width = 30,
       },
-      visible = true,
-      hide_dotfiles = true,
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          show_hidden_count = true,
+          hide_dotfiles = true,
+          hide_gitignored = true,
+          hide_by_name = {
+            -- add extension names you want to explicitly exclude
+            -- '.git',
+            -- '.DS_Store',
+            -- 'thumbs.db',
+          },
+          never_show = {},
+        },
+      },
     })
-
-    vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
   end,
 }
